64931a4bc13e9ac4841622ca235a34af06cdeab1
mwashief <muwashief@gmail.com>
2020-07-23 16:31:16 +0600

    UVa 10496 DP Solution

--- UVa/10496.cpp
+++ UVa/10496.cpp
@@ -2,31 +2,43 @@
 using namespace std;
 int beeperX[14];
 int beeperY[14];
+int memo[3000][15];
+int n, T;
+int complete;
+
+int dp(int mask, int last)
+{
+    if (mask == complete)
+        return abs(beeperX[0] - beeperX[last]) + abs(beeperY[0] - beeperY[last]);
+    if (memo[mask][last])
+        return memo[mask][last];
+
+    int result = INT32_MAX;
+    for (int i = 1; i <= n; i++)
+        if (!(mask & (1 << i)))
+            result = min(result, abs(beeperX[i] - beeperX[last]) + abs(beeperY[i] - beeperY[last]) + dp(mask | (1 << i), i));
+
+    return memo[mask][last] = result;
+}
 
 int main()
 {
-    int T;
     cin >> T;
     while (T--)
     {
+        memset(memo, 0, sizeof memo);
         cin >> beeperX[0] >> beeperY[0];
         cin >> beeperX[0] >> beeperY[0];
-        int n;
         cin >> n;
-        int arr[n + 1] = {0};
+
+        complete = 1;
+        for (int i = 0; i <= n; i++)
+            complete *= 2;
+        complete--;
+
         for (int i = 1; i <= n; i++)
-        {
             cin >> beeperX[i] >> beeperY[i];
-            arr[i] = i;
-        }
-        int result = INT32_MAX;
-        do
-        {
-            int cost = 0;
-            for (int i = 0; i <= n; i++)
-                cost += abs(beeperX[arr[i]] - beeperX[arr[(i + n) % (n + 1)]]) + abs(beeperY[arr[i]] - beeperY[arr[(i + n) % (n + 1)]]);
-            result = min(result, cost);
-        } while (next_permutation(arr + 1, arr + n + 1));
-        cout << "The shortest path has length " << result << endl;
+
+        cout << "The shortest path has length " << dp(1, 0) << endl;
     }
 }
