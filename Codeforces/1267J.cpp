b9f16749abe99909223a2cb65ce560c6ed50f3dc
mwashief <muwashief@gmail.com>
2020-08-10 02:46:12 +0600

    Reformatted

--- Codeforces/1267J.cpp
+++ Codeforces/1267J.cpp
@@ -6,45 +6,45 @@ int T, n, x;
 
 int main()
 {
-  ios_base::sync_with_stdio(0);
-  cin.tie(0);
-  cin >> T;
-  while (T--)
-  {
-    cin >> n;
-    c.clear();
-
-    for (int i = 0; i < n; i++)
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> T;
+    while (T--)
     {
-      cin >> x;
-      c[x]++;
-    }
-    int maxSize = 2000006;
-    for (auto it = c.begin(); it != c.end(); ++it)
-      maxSize = min(maxSize, it->second);
+        cin >> n;
+        c.clear();
 
-    int minScreens = 2000006;
-    for (int i = 2; i <= maxSize + 1; i++)
-    {
-      int screens = 0;
-      for (auto it = c.begin(); it != c.end(); ++it)
-      {
-        if (it->second % i == 0)
+        for (int i = 0; i < n; i++)
         {
-          screens += it->second / i;
+            cin >> x;
+            c[x]++;
         }
-        else if (it->second / i + it->second % i >= i - 1)
+        int maxSize = 2000006;
+        for (auto it = c.begin(); it != c.end(); ++it)
+            maxSize = min(maxSize, it->second);
+
+        int minScreens = 2000006;
+        for (int i = 2; i <= maxSize + 1; i++)
         {
-          screens += 1 + it->second / i;
+            int screens = 0;
+            for (auto it = c.begin(); it != c.end(); ++it)
+            {
+                if (it->second % i == 0)
+                {
+                    screens += it->second / i;
+                }
+                else if (it->second / i + it->second % i >= i - 1)
+                {
+                    screens += 1 + it->second / i;
+                }
+                else
+                {
+                    screens = 2000007;
+                    break;
+                }
+            }
+            minScreens = min(minScreens, screens);
         }
-        else
-        {
-          screens = 2000007;
-          break;
-        }
-      }
-      minScreens = min(minScreens, screens);
+        cout << minScreens << endl;
     }
-    cout << minScreens << endl;
-  }
 }
--- Codeforces/606B.cpp
+++ Codeforces/606B.cpp
@@ -8,52 +8,52 @@ int T, n, m, a, b, x, y;
 
 void dfs(int p, int q, bool *visited)
 {
-  if (visited[p])
-    return;
-  visited[p] = true;
+    if (visited[p])
+        return;
+    visited[p] = true;
 
-  for (auto it = adj[p].begin(); it != adj[p].end(); ++it)
-  {
-    dfs(*it, q, visited);
-  }
-  return;
+    for (auto it = adj[p].begin(); it != adj[p].end(); ++it)
+    {
+        dfs(*it, q, visited);
+    }
+    return;
 }
 
 int main()
 {
-  ios_base::sync_with_stdio(false);
-  cin.tie(0);
-  cin >> T;
-  while (T--)
-  {
-    cin >> n >> m >> a >> b;
-    for (int i = 1; i <= n; i++)
-      adj[i].clear();
-    for (int i = 0; i < m; i++)
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cin >> T;
+    while (T--)
     {
-      cin >> x >> y;
-      adj[x].push_back(y);
-      adj[y].push_back(x);
+        cin >> n >> m >> a >> b;
+        for (int i = 1; i <= n; i++)
+            adj[i].clear();
+        for (int i = 0; i < m; i++)
+        {
+            cin >> x >> y;
+            adj[x].push_back(y);
+            adj[y].push_back(x);
+        }
+
+        memset(visitedA, 0, n + 1);
+        visitedA[b] = true;
+        dfs(a, b, visitedA);
+        memset(visitedB, 0, n + 1);
+        visitedB[a] = true;
+        dfs(b, a, visitedB);
+
+        unsigned long long adjA = 0;
+        unsigned long long adjB = 0;
+
+        for (int i = 1; i <= n; i++)
+        {
+            if (visitedA[i] && !visitedB[i])
+                adjA++;
+            if (!visitedA[i] && visitedB[i])
+                adjB++;
+        }
+
+        cout << adjA * adjB << endl;
     }
-
-    memset(visitedA, 0, n + 1);
-    visitedA[b] = true;
-    dfs(a, b, visitedA);
-    memset(visitedB, 0, n + 1);
-    visitedB[a] = true;
-    dfs(b, a, visitedB);
-
-    unsigned long long adjA = 0;
-    unsigned long long adjB = 0;
-
-    for (int i = 1; i <= n; i++)
-    {
-      if (visitedA[i] && !visitedB[i])
-        adjA++;
-      if (!visitedA[i] && visitedB[i])
-        adjB++;
-    }
-
-    cout << adjA * adjB << endl;
-  }
 }
