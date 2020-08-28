5c74100061afa4b5bf85164cbfdd127ccf2ca547
mwashief <muwashief@gmail.com>
2020-08-28 14:15:48 +0600

    Added variables

--- editSettings.js
+++ editSettings.js
@@ -1,11 +1,16 @@
 var fs = require('fs')
 var path = require('path')
+
+var inputFile = "input.dat"
+var outputFile = "output.dat"
+
+
 fs.readFile('.vscode/settings.json', 'utf8', function(err, data) {
     if (err) {
         return console.log(err);
     }
     const viewPath = (path.join(__dirname)).replace(/\\/g, '/')
-    var result = data.replace(/\"cpp\":.*/, `"cpp" : "cd $dir && g++ -DLOCAL_OUTPUT=\\\\\\\"${viewPath}/output.dat\\\\\\\" -DLOCAL_INPUT=\\\\\\\"${viewPath}/input.dat\\\\\\\" $fileName -o $fileNameWithoutExt && $dir$fileNameWithoutExt",`);
+    var result = data.replace(/\"cpp\":.*/, `"cpp" : "cd $dir && g++ -DLOCAL_OUTPUT=\\\\\\\"${viewPath}/${outputFile}\\\\\\\" -DLOCAL_INPUT=\\\\\\\"${viewPath}/${inputFile}\\\\\\\" $fileName -o $fileNameWithoutExt && $dir$fileNameWithoutExt",`);
 
     fs.writeFile('.vscode/settings.json', result, 'utf8', function(err) {
         if (err) return console.log(err);
