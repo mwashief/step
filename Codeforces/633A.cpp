031f313856b6cacda8fa23c0c3ddd63afc92c1cb
mwashief <muwashief@gmail.com>
2020-12-24 07:55:15 +0600

    Codeforces 633A

---
+++ Codeforces/633A.cpp
@@ -0,0 +1,95 @@
+/*
+    Washief Hossain Mugdho
+    24 December 2020
+    Codeforces 633A
+*/
+
+#ifndef DEBUG
+#pragma GCC optimize("O2")
+#endif
+
+#include <bits/stdc++.h>
+#define pb push_back
+#define mp make_pair
+#define fr first
+#define sc second
+#define fastio ios_base::sync_with_stdio(0)
+#define untie cin.tie(0)
+#define rep(i, n) for (int i = 0; i < n; i++)
+#define repe(i, n) for (int i = 1; i <= n; i++)
+#define rrep(i, n) for (int i = n - 1; i >= 0; i--)
+#define rrepe(i, n) for (int i = n; i > 0; i--)
+#define ms(a, b) memset(a, b, sizeof a)
+#define MOD 1000000007
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+using namespace std;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+using vl = vector<ll>;
+using vpii = vector<pair<int, int>>;
+using mii = map<int, int>;
+
+/***********IO Utility**************/
+template <typename... ArgTypes>
+void print(ArgTypes... args);
+template <typename... ArgTypes>
+void input(ArgTypes &... args);
+template <>
+void print() {}
+template <>
+void input() {}
+template <typename T, typename... ArgTypes>
+void print(T t, ArgTypes... args)
+{
+    cout << t;
+    print(args...);
+}
+template <typename T, typename... ArgTypes>
+void input(T &t, ArgTypes &... args)
+{
+    cin >> t;
+    input(args...);
+}
+
+void run_tests()
+{
+    int n;
+    input(n);
+    vi a(n);
+    int res = 0;
+    rep(i, n) input(a[i]);
+    repe(i, n - 1) if (a[i] < a[i - 1])
+    {
+        int diff = a[i - 1] - a[i];
+        int time;
+        rrep(i, 31) if ((1 << i) <= diff)
+        {
+            time = i + 1;
+            break;
+        }
+        a[i] = a[i - 1];
+        res = max(res, time);
+    }
+    print(res, "\n");
+}
+int main()
+{
+    fastio;
+    untie;
+#ifdef LOCAL_OUTPUT
+    freopen(LOCAL_OUTPUT, "w", stdout);
+#endif
+#ifdef LOCAL_INPUT
+    freopen(LOCAL_INPUT, "r", stdin);
+#endif
+
+    int tc;
+    cin >> tc;
+    while (tc--)
+        run_tests();
+}
