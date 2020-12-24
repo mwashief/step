059149cb65f41dad0a2e9bbe6b899b4abcb6ff6f
mwashief <muwashief@gmail.com>
2020-12-24 08:47:35 +0600

    Codeforces 271D

---
+++ Codeforces/271D.cpp
@@ -0,0 +1,91 @@
+/*
+    Washief Hossain Mugdho
+    24 December 2020
+    Codeforces 271D
+*/
+
+#ifndef DEBUG
+#pragma GCC optimize("O2")
+#endif
+
+#include <bits/stdc++.h>
+#define pb push_back
+#define mp make_pair
+#define fr first
+#define sc second
+#define fastio ios_base::sync_with_stdio(0)
+#define untie cin.tie(0)
+#define rep(i, n) for (int i = 0; i < n; i++)
+#define repe(i, n) for (int i = 1; i <= n; i++)
+#define rrep(i, n) for (int i = n - 1; i >= 0; i--)
+#define rrepe(i, n) for (int i = n; i > 0; i--)
+#define ms(a, b) memset(a, b, sizeof a)
+#define MOD 1000000007
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+using namespace std;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+using vl = vector<ll>;
+using vpii = vector<pair<int, int>>;
+using mii = map<int, int>;
+
+/***********IO Utility**************/
+template <typename... ArgTypes>
+void print(ArgTypes... args);
+template <typename... ArgTypes>
+void input(ArgTypes &... args);
+template <>
+void print() {}
+template <>
+void input() {}
+template <typename T, typename... ArgTypes>
+void print(T t, ArgTypes... args)
+{
+    cout << t;
+    print(args...);
+}
+template <typename T, typename... ArgTypes>
+void input(T &t, ArgTypes &... args)
+{
+    cin >> t;
+    input(args...);
+}
+int q, k;
+vi memo(100009, -1);
+
+int dp(int flowers)
+{
+    if (flowers < 0)
+        return 0;
+    if (memo[flowers] != -1)
+        return memo[flowers];
+    return memo[flowers] = (dp(flowers - 1) + dp(flowers - k)) % MOD;
+}
+
+int main()
+{
+    fastio;
+    untie;
+#ifdef LOCAL_OUTPUT
+    freopen(LOCAL_OUTPUT, "w", stdout);
+#endif
+#ifdef LOCAL_INPUT
+    freopen(LOCAL_INPUT, "r", stdin);
+#endif
+    input(q, k);
+    memo[0] = 1;
+    dp(100005);
+    repe(i, 100005)
+        memo[i] = (memo[i - 1] + memo[i]) % MOD;
+    rep(i, q)
+    {
+        int a, b;
+        input(a, b);
+        print((ll)(memo[b] - memo[a - 1] + MOD) % MOD, "\n");
+    }
+}
