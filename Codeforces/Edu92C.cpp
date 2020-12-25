e64a715509d97ce1f84121bde6093e7e9433dfbd
mwashief <muwashief@gmail.com>
2020-12-25 11:58:42 +0600

    Codeforces Edu92C

---
+++ Codeforces/Edu92C.cpp
@@ -0,0 +1,100 @@
+/*
+    Washief Hossain Mugdho
+    25 December 2020
+    Codeforces Edu92C
+*/
+
+#ifndef DEBUG
+#pragma GCC optimize("O2")
+#endif
+
+#include <bits/stdc++.h>
+#define pb push_back
+#define mp make_pair
+#define fr first
+#define sc second
+#define fastio ios_base::sync_with_stdio(0)
+#define untie cin.tie(0)
+#define rep(i, n) for (int i = 0; i < n; i++)
+#define repe(i, n) for (int i = 1; i <= n; i++)
+#define rrep(i, n) for (int i = n - 1; i >= 0; i--)
+#define rrepe(i, n) for (int i = n; i > 0; i--)
+#define ms(a, b) memset(a, b, sizeof a)
+#define MOD 1000000007
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+using namespace std;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+using vl = vector<ll>;
+using vpii = vector<pair<int, int>>;
+using mii = map<int, int>;
+
+/***********IO Utility**************/
+template <typename... ArgTypes>
+void print(ArgTypes... args);
+template <typename... ArgTypes>
+void input(ArgTypes &... args);
+template <>
+void print() {}
+template <>
+void input() {}
+template <typename T, typename... ArgTypes>
+void print(T t, ArgTypes... args)
+{
+    cout << t;
+    print(args...);
+}
+template <typename T, typename... ArgTypes>
+void input(T &t, ArgTypes &... args)
+{
+    cin >> t;
+    input(args...);
+}
+string s;
+
+void run_tests()
+{
+    cin >> s;
+    int res = INT32_MAX;
+    rep(i, 10) rep(j, 10)
+    {
+        bool which = false;
+        int escaped = 0;
+        rep(k, s.size())
+        {
+            if ((which && s[k] == '0' + j) || (!which && s[k] == '0' + i))
+                which ^= 1;
+            else
+                escaped++;
+        }
+        int rem = s.size() - escaped;
+        if (rem == 0)
+            continue;
+        if (i != j && rem & 1)
+            escaped++;
+        res = min(res, escaped);
+    }
+    cout << res << endl;
+}
+
+int main()
+{
+    fastio;
+    untie;
+#ifdef LOCAL_OUTPUT
+    freopen(LOCAL_OUTPUT, "w", stdout);
+#endif
+#ifdef LOCAL_INPUT
+    freopen(LOCAL_INPUT, "r", stdin);
+#endif
+
+    int tc;
+    cin >> tc;
+    while (tc--)
+        run_tests();
+}
