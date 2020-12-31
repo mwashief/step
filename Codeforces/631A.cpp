55c04c7bb9098da33f39a74a8d5c9d7f394895e2
mwashief <muwashief@gmail.com>
2021-01-01 05:13:33 +0600

    Codeforces 631A

---
+++ Codeforces/631A.cpp
@@ -0,0 +1,99 @@
+/*
+    Washief Hossain Mugdho
+    01 January 2021
+    Codeforces 631A
+*/
+
+#ifndef DEBUG
+#pragma GCC optimize("O2")
+#endif
+
+#include <bits/stdc++.h>
+#define pb push_back
+#define mp make_pair
+#define fr first
+#define sc second
+#define fastio ios_base::sync_with_stdio(0)
+#define untie cin.tie(0)
+#define rep(i, n) for (int i = 0; i < n; i++)
+#define repe(i, n) for (int i = 1; i <= n; i++)
+#define rrep(i, n) for (int i = n - 1; i >= 0; i--)
+#define rrepe(i, n) for (int i = n; i > 0; i--)
+#define ms(a, b) memset(a, b, sizeof a)
+#define MOD 1000000007
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+using namespace std;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vb = vector<bool>;
+using vi = vector<int>;
+using vl = vector<ll>;
+using vvb = vector<vector<bool>>;
+using vvi = vector<vector<int>>;
+using vvl = vector<vector<ll>>;
+using vpii = vector<pair<int, int>>;
+using mii = map<int, int>;
+
+/***********IO Utility**************/
+template <typename... ArgTypes>
+void print(ArgTypes... args);
+template <typename... ArgTypes>
+void input(ArgTypes &... args);
+template <>
+void print() {}
+template <>
+void input() {}
+template <typename T, typename... ArgTypes>
+void print(T t, ArgTypes... args)
+{
+    cout << t;
+    print(args...);
+}
+template <typename T, typename... ArgTypes>
+void input(T &t, ArgTypes &... args)
+{
+    cin >> t;
+    input(args...);
+}
+
+int main()
+{
+    fastio;
+    untie;
+#ifdef LOCAL_OUTPUT
+    freopen(LOCAL_OUTPUT, "w", stdout);
+#endif
+#ifdef LOCAL_INPUT
+    freopen(LOCAL_INPUT, "r", stdin);
+#endif
+
+    ll n, m, impossible = 0;
+    cin >> n >> m;
+    vl l(m), sum(m + 1, 0);
+    rep(i, m)
+    {
+        cin >> l[i];
+        sum[i + 1] = sum[i] + l[i];
+        if (l[i] + i > n)
+            impossible = 1;
+    }
+    if (impossible || sum[m] < n)
+    {
+        cout << -1 << endl;
+        return 0;
+    }
+    int last = 1;
+    for (int i = 0; i < m; i++)
+    {
+        cout << last << " ";
+        ll restSum = sum[m] - sum[i + 1];
+        if (restSum + last >= n)
+            last++;
+        else
+            last = n - restSum + 1;
+    }
+    cout << endl;
+}
