420708d3c4c1e6531cee3e95fab5f00ab4ca2971
mwashief <muwashief@gmail.com>
2021-01-07 16:31:44 +0600

    Codeforces 1321C

---
+++ Codeforces/1321C.cpp
@@ -0,0 +1,115 @@
+/*
+    Washief Hossain Mugdho
+    07 January 2021
+    Codeforces 1321C
+*/
+
+#ifndef DEBUG
+#pragma GCC optimize("O2")
+#endif
+
+#include <bits/stdc++.h>
+#define pb push_back
+#define mp make_pair
+#define fr first
+#define sc second
+#define fastio ios_base::sync_with_stdio(0)
+#define untie cin.tie(0)
+#define rep(i, n) for (int i = 0; i < n; i++)
+#define repe(i, n) for (int i = 1; i <= n; i++)
+#define rrep(i, n) for (int i = n - 1; i >= 0; i--)
+#define rrepe(i, n) for (int i = n; i > 0; i--)
+#define ms(a, b) memset(a, b, sizeof a)
+#define MOD 1000000007
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+using namespace std;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vb = vector<bool>;
+using vi = vector<int>;
+using vl = vector<ll>;
+using vvb = vector<vector<bool>>;
+using vvi = vector<vector<int>>;
+using vvl = vector<vector<ll>>;
+using vpii = vector<pair<int, int>>;
+using mii = map<int, int>;
+
+/***********IO Utility**************/
+template <typename... ArgTypes>
+void print(ArgTypes... args);
+template <typename... ArgTypes>
+void input(ArgTypes &... args);
+template <>
+void print() {}
+template <>
+void input() {}
+template <typename T, typename... ArgTypes>
+void print(T t, ArgTypes... args)
+{
+    cout << t;
+    print(args...);
+}
+template <typename T, typename... ArgTypes>
+void input(T &t, ArgTypes &... args)
+{
+    cin >> t;
+    input(args...);
+}
+
+list<char> l;
+list<char>::iterator ff(list<char>::iterator start, char d)
+{
+    for (auto i = start; i != l.end(); i++)
+        if (*i == d)
+            return i;
+    return l.end();
+}
+
+int main()
+{
+    fastio;
+    untie;
+#ifdef LOCAL_OUTPUT
+    freopen(LOCAL_OUTPUT, "w", stdout);
+#endif
+#ifdef LOCAL_INPUT
+    freopen(LOCAL_INPUT, "r", stdin);
+#endif
+    int n;
+    cin >> n;
+    rep(i, n)
+    {
+        char d;
+        cin >> d;
+        l.push_back(d);
+    }
+    int ans = 0;
+    for (char i = 'z'; i >= 'b'; i--)
+    {
+        list<char>::iterator start = l.begin();
+        while (1)
+        {
+            auto pos = ff(start, i);
+            if (pos == l.end())
+                break;
+            if (pos != l.begin() && *prev(pos) == i - 1)
+            {
+                l.erase(pos);
+                ans++;
+                start = l.begin();
+            }
+            else if (pos != prev(l.end()) && *next(pos) == i - 1)
+            {
+                l.erase(pos);
+                ans++;
+                start = l.begin();
+            }
+            else
+                start = next(pos);
+        }
+    }
+    cout << ans << endl;
+}
