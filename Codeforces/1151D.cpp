fffb0eb2a14a12958050a125897aefe1330d64be
mwashief <muwashief@gmail.com>
2021-01-10 18:53:30 +0600

    Codeforces 1151D

---
+++ Codeforces/1151D.cpp
@@ -0,0 +1,89 @@
+/*
+    Washief Hossain Mugdho
+    10 January 2021
+    Codeforces 1151D
+*/
+
+#ifndef DEBUG
+#pragma GCC optimize("O2")
+#endif
+
+#include <bits/stdc++.h>
+#define pb push_back
+#define mp make_pair
+#define fr first
+#define sc second
+#define fastio ios_base::sync_with_stdio(0)
+#define untie cin.tie(0)
+#define rep(i, n) for (int i = 0; i < n; i++)
+#define repe(i, n) for (int i = 1; i <= n; i++)
+#define rrep(i, n) for (int i = n - 1; i >= 0; i--)
+#define rrepe(i, n) for (int i = n; i > 0; i--)
+#define ms(a, b) memset(a, b, sizeof a)
+#define MOD 1000000007
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+using namespace std;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vb = vector<bool>;
+using vi = vector<int>;
+using vl = vector<ll>;
+using vvb = vector<vector<bool>>;
+using vvi = vector<vector<int>>;
+using vvl = vector<vector<ll>>;
+using vpii = vector<pair<int, int>>;
+using mii = map<int, int>;
+
+/***********IO Utility**************/
+template <typename... ArgTypes>
+void print(ArgTypes... args);
+template <typename... ArgTypes>
+void input(ArgTypes &... args);
+template <>
+void print() {}
+template <>
+void input() {}
+template <typename T, typename... ArgTypes>
+void print(T t, ArgTypes... args)
+{
+    cout << t;
+    print(args...);
+}
+template <typename T, typename... ArgTypes>
+void input(T &t, ArgTypes &... args)
+{
+    cin >> t;
+    input(args...);
+}
+
+vl a, b;
+int main()
+{
+    fastio;
+    untie;
+#ifdef LOCAL_OUTPUT
+    freopen(LOCAL_OUTPUT, "w", stdout);
+#endif
+#ifdef LOCAL_INPUT
+    freopen(LOCAL_INPUT, "r", stdin);
+#endif
+    int n;
+    cin >> n;
+    a = vl(n);
+    b = vl(n);
+    vi order;
+    rep(i, n)
+    {
+        cin >> a[i] >> b[i];
+        order.pb(i);
+    }
+    sort(order.begin(), order.end(), [](int x, int y) {
+        return b[x] + a[y] < a[x] + b[y];
+    });
+    ll res = 0;
+    rep(i, n) res += i * a[order[i]] + (n - i - 1) * b[order[i]];
+    cout << res << endl;
+}
