9fedf6bc4b0171c54250a9e75ea4fbb9a863b88e
mwashief <muwashief@gmail.com>
2021-01-13 22:27:37 +0600

    Codeforces 1036B

---
+++ Codeforces/1036B.cpp
@@ -0,0 +1,110 @@
+/*
+    Washief Hossain Mugdho
+    13 January 2021
+    Codeforces 1036B
+*/
+
+#ifndef DEBUG
+#pragma GCC optimize("O2")
+#endif
+
+#include <bits/stdc++.h>
+#define pb push_back
+#define mp make_pair
+#define fr first
+#define sc second
+#define fastio ios_base::sync_with_stdio(0)
+#define untie cin.tie(0)
+#define rep(i, n) for (int i = 0; i < n; i++)
+#define repe(i, n) for (int i = 1; i <= n; i++)
+#define rrep(i, n) for (int i = n - 1; i >= 0; i--)
+#define rrepe(i, n) for (int i = n; i > 0; i--)
+#define ms(a, b) memset(a, b, sizeof a)
+#define MOD 1000000007
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+using namespace std;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vb = vector<bool>;
+using vi = vector<int>;
+using vl = vector<ll>;
+using vvb = vector<vector<bool>>;
+using vvi = vector<vector<int>>;
+using vvl = vector<vector<ll>>;
+using vpii = vector<pair<int, int>>;
+using mii = map<int, int>;
+
+/***********IO Utility**************/
+template <typename... ArgTypes>
+void print(ArgTypes... args);
+template <typename... ArgTypes>
+void input(ArgTypes &... args);
+template <>
+void print() {}
+template <>
+void input() {}
+template <typename T, typename... ArgTypes>
+void print(T t, ArgTypes... args)
+{
+    cout << t;
+    print(args...);
+}
+template <typename T, typename... ArgTypes>
+void input(T &t, ArgTypes &... args)
+{
+    cin >> t;
+    input(args...);
+}
+
+inline void run_tests()
+{
+    ll n, m, k;
+    cin >> n >> m >> k;
+    ll minimumMoves = max(n, m);
+    if (minimumMoves > k)
+    {
+        cout << -1 << endl;
+        return;
+    }
+    ll diagonal = min(n, m);
+    ll straight = minimumMoves - diagonal;
+    if (straight & 1)
+    {
+        k -= diagonal;
+        ll taken = min(k - 1, straight - 1);
+        k -= taken;
+        cout << diagonal + taken + k - 1 << endl;
+    }
+    else
+    {
+        k -= diagonal;
+        ll taken = min(k, straight);
+        k -= taken;
+        if (k % 2 == 0)
+        {
+            cout << diagonal + taken + k << endl;
+            return;
+        }
+        cout << diagonal + taken + k - 2 << endl;
+    }
+}
+
+int main()
+{
+    fastio;
+    untie;
+#ifdef LOCAL_OUTPUT
+    freopen(LOCAL_OUTPUT, "w", stdout);
+#endif
+#ifdef LOCAL_INPUT
+    freopen(LOCAL_INPUT, "r", stdin);
+#endif
+
+    int tc;
+    cin >> tc;
+    while (tc--)
+        run_tests();
+}
