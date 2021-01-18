5339f767d3182ed15b7c594f158dc757bc225c1f
mwashief <muwashief@gmail.com>
2021-01-18 17:27:45 +0600

    Codeforces 1368C

---
+++ Codeforces/1368D.cpp
@@ -0,0 +1,95 @@
+/*
+    Washief Hossain Mugdho
+    18 January 2021
+    Codeforces 1368D
+*/
+
+#ifndef DEBUG
+#pragma GCC optimize("O2")
+#endif
+
+#include <bits/stdc++.h>
+#define pb push_back
+#define mp make_pair
+#define fr first
+#define sc second
+#define fastio ios_base::sync_with_stdio(0)
+#define untie cin.tie(0)
+#define rep(i, n) for (int i = 0; i < n; i++)
+#define repe(i, n) for (int i = 1; i <= n; i++)
+#define rrep(i, n) for (int i = n - 1; i >= 0; i--)
+#define rrepe(i, n) for (int i = n; i > 0; i--)
+#define ms(a, b) memset(a, b, sizeof a)
+#define MOD 1000000007
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+using namespace std;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vb = vector<bool>;
+using vi = vector<int>;
+using vl = vector<ll>;
+using vvb = vector<vector<bool>>;
+using vvi = vector<vector<int>>;
+using vvl = vector<vector<ll>>;
+using vpii = vector<pair<int, int>>;
+using mii = map<int, int>;
+
+/***********IO Utility**************/
+template <typename... ArgTypes>
+void print(ArgTypes... args);
+template <typename... ArgTypes>
+void input(ArgTypes &...args);
+template <>
+void print() {}
+template <>
+void input() {}
+template <typename T, typename... ArgTypes>
+void print(T t, ArgTypes... args)
+{
+    cout << t;
+    print(args...);
+}
+template <typename T, typename... ArgTypes>
+void input(T &t, ArgTypes &...args)
+{
+    cin >> t;
+    input(args...);
+}
+
+int main()
+{
+    fastio;
+#ifdef LOCAL_OUTPUT
+    freopen(LOCAL_OUTPUT, "w", stdout);
+#endif
+#ifdef LOCAL_INPUT
+    freopen(LOCAL_INPUT, "r", stdin);
+#endif
+
+    int n;
+    cin >> n;
+    vi a(22, 0);
+    rep(i, n)
+    {
+        ll x;
+        cin >> x;
+        rep(j, 22) if (x & (1 << j)) a[j]++;
+    }
+    ll ans = 0;
+    while (1)
+    {
+        ll x = 0;
+        rep(i, 22) if (a[i])
+        {
+            x |= (1LL << i);
+            a[i]--;
+        }
+        if (x == 0LL)
+            break;
+        ans += x * x;
+    }
+    cout << ans << endl;
+}
