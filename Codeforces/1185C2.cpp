3418402810f10bdd6f53e8613ffffc1365a609bb
mwashief <muwashief@gmail.com>
2021-01-26 13:35:21 +0600

    Codeforces 1185C2

---
+++ Codeforces/1185C2.cpp
@@ -0,0 +1,92 @@
+/*
+    Washief Hossain Mugdho
+    26 January 2021
+    Codeforces 1185C2
+*/
+
+#ifndef DEBUG
+#pragma GCC optimize("O2")
+#endif
+
+#include <bits/stdc++.h>
+#define pb push_back
+#define mp make_pair
+#define fr first
+#define sc second
+#define fastio ios_base::sync_with_stdio(0)
+#define untie cin.tie(0)
+#define rep(i, n) for (int i = 0; i < n; i++)
+#define repe(i, n) for (int i = 1; i <= n; i++)
+#define rrep(i, n) for (int i = n - 1; i >= 0; i--)
+#define rrepe(i, n) for (int i = n; i > 0; i--)
+#define ms(a, b) memset(a, b, sizeof a)
+#define MOD 1000000007
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+using namespace std;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vb = vector<bool>;
+using vi = vector<int>;
+using vl = vector<ll>;
+using vvb = vector<vector<bool>>;
+using vvi = vector<vector<int>>;
+using vvl = vector<vector<ll>>;
+using vpii = vector<pair<int, int>>;
+using mii = map<int, int>;
+
+/***********IO Utility**************/
+template <typename... ArgTypes>
+void print(ArgTypes... args);
+template <typename... ArgTypes>
+void input(ArgTypes &...args);
+template <>
+void print() {}
+template <>
+void input() {}
+template <typename T, typename... ArgTypes>
+void print(T t, ArgTypes... args)
+{
+    cout << t;
+    print(args...);
+}
+template <typename T, typename... ArgTypes>
+void input(T &t, ArgTypes &...args)
+{
+    cin >> t;
+    input(args...);
+}
+
+int main()
+{
+    fastio;
+#ifdef LOCAL_OUTPUT
+    freopen(LOCAL_OUTPUT, "w", stdout);
+#endif
+#ifdef LOCAL_INPUT
+    freopen(LOCAL_INPUT, "r", stdin);
+#endif
+    int n, m;
+    cin >> n >> m;
+    mii soFar;
+    int total = 0;
+    rep(i, n)
+    {
+        int x;
+        cin >> x;
+        int rest = m - x;
+        int res = 0;
+        for (auto entry : soFar)
+        {
+            int num = entry.fr;
+            int contain = entry.sc;
+            int take = min(contain, rest / num);
+            rest -= take * num;
+            res += take;
+        }
+        cout << i - res << " ";
+        soFar[x]++;
+    }
+}
