05c820ce7a5ee4c25e699e2b35127f8146c48cd0
mwashief <muwashief@gmail.com>
2021-01-31 15:10:12 +0600

    Codeforces 1392E

---
+++ Codeforces/1392E.cpp
@@ -0,0 +1,106 @@
+/*
+    Washief Hossain Mugdho
+    31 January 2021
+    Codeforces 1392E
+*/
+
+#ifndef DEBUG
+#pragma GCC optimize("O2")
+#endif
+
+#include <bits/stdc++.h>
+#define pb push_back
+#define mp make_pair
+#define fr first
+#define sc second
+#define fastio ios_base::sync_with_stdio(0)
+#define untie cin.tie(0)
+#define rep(i, n) for (int i = 0; i < n; i++)
+#define repe(i, n) for (int i = 1; i <= n; i++)
+#define rrep(i, n) for (int i = n - 1; i >= 0; i--)
+#define rrepe(i, n) for (int i = n; i > 0; i--)
+#define ms(a, b) memset(a, b, sizeof a)
+#define MOD 1000000007
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+using namespace std;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vb = vector<bool>;
+using vi = vector<int>;
+using vl = vector<ll>;
+using vvb = vector<vector<bool>>;
+using vvi = vector<vector<int>>;
+using vvl = vector<vector<ll>>;
+using vpii = vector<pair<int, int>>;
+using mii = map<int, int>;
+
+/***********IO Utility**************/
+template <typename... ArgTypes>
+void print(ArgTypes... args);
+template <typename... ArgTypes>
+void input(ArgTypes &...args);
+template <>
+void print() {}
+template <>
+void input() {}
+template <typename T, typename... ArgTypes>
+void print(T t, ArgTypes... args)
+{
+    cout << t;
+    print(args...);
+}
+template <typename T, typename... ArgTypes>
+void input(T &t, ArgTypes &...args)
+{
+    cin >> t;
+    input(args...);
+}
+
+int main()
+{
+    fastio;
+#ifdef LOCAL_OUTPUT
+    freopen(LOCAL_OUTPUT, "w", stdout);
+#endif
+#ifdef LOCAL_INPUT
+    freopen(LOCAL_INPUT, "r", stdin);
+#endif
+
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (i & 1)
+                cout << 0 << " ";
+            else
+                cout << (1LL << (i + j)) << " ";
+        }
+        cout << endl;
+    }
+    int q;
+    cin >> q;
+    rep(k, q)
+    {
+        ll sum;
+        int x = 1;
+        int y = 1;
+        cin >> sum;
+        cout << x << " " << y << endl;
+        for (int i = 0; i < 2 * n - 2; i++)
+        {
+            bool now = sum & 1LL;
+            bool next = sum & 2LL;
+            if (now == next)
+                y++;
+            else
+                x++;
+            sum >>= 1;
+            cout << x << " " << y << endl;
+        }
+    }
+}
