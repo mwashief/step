d7175472449686a73c2d139cb90f4b3086a7dbe2
mwashief <muwashief@gmail.com>
2021-02-01 20:31:33 +0600

    Codeforces 1371E1

---
+++ Codeforces/1371E1.cpp
@@ -0,0 +1,98 @@
+/*
+    Washief Hossain Mugdho
+    01 February 2021
+    Codeforces 1371E1
+*/
+
+#ifndef DEBUG
+#pragma GCC optimize("O2")
+#endif
+
+#include <bits/stdc++.h>
+#define pb push_back
+#define mp make_pair
+#define fr first
+#define sc second
+#define fastio ios_base::sync_with_stdio(0)
+#define untie cin.tie(0)
+#define rep(i, n) for (int i = 0; i < n; i++)
+#define repe(i, n) for (int i = 1; i <= n; i++)
+#define rrep(i, n) for (int i = n - 1; i >= 0; i--)
+#define rrepe(i, n) for (int i = n; i > 0; i--)
+#define ms(a, b) memset(a, b, sizeof a)
+#define MOD 1000000007
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+using namespace std;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vb = vector<bool>;
+using vi = vector<int>;
+using vl = vector<ll>;
+using vvb = vector<vector<bool>>;
+using vvi = vector<vector<int>>;
+using vvl = vector<vector<ll>>;
+using vpii = vector<pair<int, int>>;
+using mii = map<int, int>;
+
+/***********IO Utility**************/
+template <typename... ArgTypes>
+void print(ArgTypes... args);
+template <typename... ArgTypes>
+void input(ArgTypes &...args);
+template <>
+void print() {}
+template <>
+void input() {}
+template <typename T, typename... ArgTypes>
+void print(T t, ArgTypes... args)
+{
+    cout << t;
+    print(args...);
+}
+template <typename T, typename... ArgTypes>
+void input(T &t, ArgTypes &...args)
+{
+    cin >> t;
+    input(args...);
+}
+ll n, p;
+vi a;
+
+bool f(int x)
+{
+    for (ll i = 0; i < n; i++)
+    {
+        ll ins = upper_bound(a.begin(), a.end(), x + i) - a.begin();
+        ins -= i;
+        if (ins <= 0LL)
+            return false;
+        if (ins % p == 0)
+            return false;
+    }
+    return true;
+}
+
+int main()
+{
+    fastio;
+#ifdef LOCAL_OUTPUT
+    freopen(LOCAL_OUTPUT, "w", stdout);
+#endif
+#ifdef LOCAL_INPUT
+    freopen(LOCAL_INPUT, "r", stdin);
+#endif
+    cin >> n >> p;
+    a = vi(n);
+    rep(i, n) cin >> a[i];
+    sort(a.begin(), a.end());
+    vi res;
+    rep(i, 2001) if (f(i))
+        res.pb(i);
+    cout << res.size() << endl;
+    for (auto r : res)
+        cout << r << " ";
+    cout << endl;
+}
