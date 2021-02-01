688fc7010384fa7284bb5ff1f7cb50ec0fc11f27
mwashief <muwashief@gmail.com>
2021-02-01 21:20:27 +0600

    Codeforces 1369D

---
+++ Codeforces/1369D.cpp
@@ -0,0 +1,103 @@
+/*
+    Washief Hossain Mugdho
+    01 February 2021
+    Codeforces 1369D
+*/
+
+#ifndef DEBUG
+#pragma GCC optimize("O2")
+#endif
+
+#include <bits/stdc++.h>
+#define pb push_back
+#define mp make_pair
+#define fr first
+#define sc second
+#define fastio ios_base::sync_with_stdio(0)
+#define untie cin.tie(0)
+#define rep(i, n) for (int i = 0; i < n; i++)
+#define repe(i, n) for (int i = 1; i <= n; i++)
+#define rrep(i, n) for (int i = n - 1; i >= 0; i--)
+#define rrepe(i, n) for (int i = n; i > 0; i--)
+#define ms(a, b) memset(a, b, sizeof a)
+#define MOD 1000000007
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+using namespace std;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vb = vector<bool>;
+using vi = vector<int>;
+using vl = vector<ll>;
+using vvb = vector<vector<bool>>;
+using vvi = vector<vector<int>>;
+using vvl = vector<vector<ll>>;
+using vpii = vector<pair<int, int>>;
+using mii = map<int, int>;
+
+/***********IO Utility**************/
+template <typename... ArgTypes>
+void print(ArgTypes... args);
+template <typename... ArgTypes>
+void input(ArgTypes &...args);
+template <>
+void print() {}
+template <>
+void input() {}
+template <typename T, typename... ArgTypes>
+void print(T t, ArgTypes... args)
+{
+    cout << t;
+    print(args...);
+}
+template <typename T, typename... ArgTypes>
+void input(T &t, ArgTypes &...args)
+{
+    cin >> t;
+    input(args...);
+}
+int memo[2000006];
+bool top[2000006];
+
+int dp(int k)
+{
+    if (k <= 2)
+        return 0;
+    if (memo[k] != -1)
+        return memo[k];
+    int temp = (2 * dp(k - 2)) % MOD;
+    temp = (temp + dp(k - 1)) % MOD;
+    bool nowTop = top[k - 1] | top[k - 2];
+    if (!nowTop)
+    {
+        top[k] = 1;
+        temp = (temp + 4) % MOD;
+    }
+    return memo[k] = temp;
+}
+
+inline void _()
+{
+    int n;
+    cin >> n;
+    cout << dp(n) << endl;
+}
+
+int main()
+{
+    fastio;
+#ifdef LOCAL_OUTPUT
+    freopen(LOCAL_OUTPUT, "w", stdout);
+#endif
+#ifdef LOCAL_INPUT
+    freopen(LOCAL_INPUT, "r", stdin);
+#endif
+
+    ms(memo, -1);
+    int __;
+    cin >> __;
+    while (__--)
+        _();
+}
