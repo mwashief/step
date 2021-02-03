304ddfecca45838d7f238f85251f04bb734583c4
mwashief <muwashief@gmail.com>
2021-02-03 22:02:07 +0600

    Codeforces 1349B

---
+++ Codeforces/1349B.cpp
@@ -0,0 +1,113 @@
+/*
+    Washief Hossain Mugdho
+    03 February 2021
+    Codeforces 1349B
+*/
+
+#ifndef DEBUG
+#pragma GCC optimize("O2")
+#endif
+
+#include <bits/stdc++.h>
+#define pb push_back
+#define mp make_pair
+#define fr first
+#define sc second
+#define fastio ios_base::sync_with_stdio(0)
+#define untie cin.tie(0)
+#define rep(i, n) for (int i = 0; i < n; i++)
+#define repe(i, n) for (int i = 1; i <= n; i++)
+#define rrep(i, n) for (int i = n - 1; i >= 0; i--)
+#define rrepe(i, n) for (int i = n; i > 0; i--)
+#define ms(a, b) memset(a, b, sizeof a)
+#define MOD 1000000007
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+using namespace std;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vb = vector<bool>;
+using vi = vector<int>;
+using vl = vector<ll>;
+using vvb = vector<vector<bool>>;
+using vvi = vector<vector<int>>;
+using vvl = vector<vector<ll>>;
+using vpii = vector<pair<int, int>>;
+using mii = map<int, int>;
+
+/***********IO Utility**************/
+template <typename... ArgTypes>
+void print(ArgTypes... args);
+template <typename... ArgTypes>
+void input(ArgTypes &...args);
+template <>
+void print() {}
+template <>
+void input() {}
+template <typename T, typename... ArgTypes>
+void print(T t, ArgTypes... args)
+{
+    cout << t;
+    print(args...);
+}
+template <typename T, typename... ArgTypes>
+void input(T &t, ArgTypes &...args)
+{
+    cin >> t;
+    input(args...);
+}
+
+inline void _()
+{
+    int n, k;
+    cin >> n >> k;
+    vi a(n);
+    bool contains = 0;
+    rep(i, n)
+    {
+        cin >> a[i];
+        if (a[i] == k)
+            contains = 1;
+    }
+    if (!contains)
+    {
+        cout << "no" << endl;
+        return;
+    }
+    if (n == 1)
+    {
+        cout << "yes" << endl;
+        return;
+    }
+    for (int i = 0; i < n - 1; i++)
+        if (a[i] >= k && a[i + 1] >= k)
+        {
+            cout << "yes" << endl;
+            return;
+        }
+    for (int i = 0; i < n - 2; i++)
+        if (a[i] >= k && a[i + 2] >= k)
+        {
+            cout << "yes" << endl;
+            return;
+        }
+    cout << "no" << endl;
+}
+
+int main()
+{
+    fastio;
+#ifdef LOCAL_OUTPUT
+    freopen(LOCAL_OUTPUT, "w", stdout);
+#endif
+#ifdef LOCAL_INPUT
+    freopen(LOCAL_INPUT, "r", stdin);
+#endif
+
+    int __;
+    cin >> __;
+    while (__--)
+        _();
+}
