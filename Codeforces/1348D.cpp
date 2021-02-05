e16ddd7517ce0dcfd73c15cbd64270b27f5c143c
mwashief <muwashief@gmail.com>
2021-02-05 09:07:30 +0600

    Codeforces 1348D

---
+++ Codeforces/1348D.cpp
@@ -0,0 +1,104 @@
+/*
+    Washief Hossain Mugdho
+    04 February 2021
+    Codeforces 1348D
+*/
+
+#ifndef DEBUG
+#pragma GCC optimize("O2")
+#endif
+
+#include <bits/stdc++.h>
+#define pb push_back
+#define mp make_pair
+#define fr first
+#define sc second
+#define fastio ios_base::sync_with_stdio(0)
+#define untie cin.tie(0)
+#define rep(i, n) for (int i = 0; i < n; i++)
+#define repe(i, n) for (int i = 1; i <= n; i++)
+#define rrep(i, n) for (int i = n - 1; i >= 0; i--)
+#define rrepe(i, n) for (int i = n; i > 0; i--)
+#define ms(a, b) memset(a, b, sizeof a)
+#define MOD 1000000007
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+using namespace std;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vb = vector<bool>;
+using vi = vector<int>;
+using vl = vector<ll>;
+using vvb = vector<vector<bool>>;
+using vvi = vector<vector<int>>;
+using vvl = vector<vector<ll>>;
+using vpii = vector<pair<int, int>>;
+using mii = map<int, int>;
+
+/***********IO Utility**************/
+template <typename... ArgTypes>
+void print(ArgTypes... args);
+template <typename... ArgTypes>
+void input(ArgTypes &...args);
+template <>
+void print() {}
+template <>
+void input() {}
+template <typename T, typename... ArgTypes>
+void print(T t, ArgTypes... args)
+{
+    cout << t;
+    print(args...);
+}
+template <typename T, typename... ArgTypes>
+void input(T &t, ArgTypes &...args)
+{
+    cin >> t;
+    input(args...);
+}
+
+inline void _()
+{
+    vi res;
+    res.pb(1);
+    int n;
+    cin >> n;
+    n--;
+    for (int i = 1; n > 0; i++)
+    {
+        int take = min(n, 1 << i);
+        n -= take;
+        res.pb(take);
+    }
+    if (res.size() > 2)
+    {
+        if (res.back() < res[res.size() - 2])
+        {
+            int total = res[res.size() - 1] + res[res.size() - 2];
+            res.back() = (total + 1) / 2;
+            res[res.size() - 2] = total / 2;
+        }
+    }
+    cout << res.size() - 1 << endl;
+    for (int i = 1; i < res.size(); i++)
+        cout << res[i] - res[i - 1] << " ";
+    cout << endl;
+}
+
+int main()
+{
+    fastio;
+#ifdef LOCAL_OUTPUT
+    freopen(LOCAL_OUTPUT, "w", stdout);
+#endif
+#ifdef LOCAL_INPUT
+    freopen(LOCAL_INPUT, "r", stdin);
+#endif
+
+    int __;
+    cin >> __;
+    while (__--)
+        _();
+}
