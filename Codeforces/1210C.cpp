c44c22e7cc4225b9aa89ad4b45477c761d74f41a
mwashief <muwashief@gmail.com>
2021-02-14 19:36:46 +0600

    Codeforces 1210C

---
+++ Codeforces/1210C.cpp
@@ -0,0 +1,107 @@
+/*
+    Washief Hossain Mugdho
+    14 February 2021
+    Codeforces 1210C
+*/
+
+#ifndef DEBUG
+#pragma GCC optimize("O2")
+#endif
+
+#include <bits/stdc++.h>
+#define pb push_back
+#define mp make_pair
+#define fr first
+#define sc second
+#define fastio ios_base::sync_with_stdio(0)
+#define untie cin.tie(0)
+#define rep(i, n) for (int i = 0; i < n; i++)
+#define repe(i, n) for (int i = 1; i <= n; i++)
+#define rrep(i, n) for (int i = n - 1; i >= 0; i--)
+#define rrepe(i, n) for (int i = n; i > 0; i--)
+#define ms(a, b) memset(a, b, sizeof a)
+#define MOD 1000000007
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+using namespace std;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vb = vector<bool>;
+using vi = vector<int>;
+using vl = vector<ll>;
+using vvb = vector<vector<bool>>;
+using vvi = vector<vector<int>>;
+using vvl = vector<vector<ll>>;
+using vpii = vector<pair<int, int>>;
+using mii = map<int, int>;
+using mli = map<ll, int>;
+
+/***********IO Utility**************/
+template <typename... ArgTypes>
+void print(ArgTypes... args);
+template <typename... ArgTypes>
+void input(ArgTypes &...args);
+template <>
+void print() {}
+template <>
+void input() {}
+template <typename T, typename... ArgTypes>
+void print(T t, ArgTypes... args)
+{
+    cout << t;
+    print(args...);
+}
+template <typename T, typename... ArgTypes>
+void input(T &t, ArgTypes &...args)
+{
+    cin >> t;
+    input(args...);
+}
+int n;
+ll ans = 0;
+vl cost;
+vvi adj;
+
+void dfs(int node, int parent, mli paths)
+{
+    mli temp;
+    ans = (ans + cost[node]) % MOD;
+    temp[cost[node]]++;
+    for (auto x : paths)
+    {
+        ll g = __gcd(x.fr, cost[node]);
+        ans = (ans + g * x.sc % MOD) % MOD;
+        temp[g] += x.sc;
+    }
+    for (auto x : adj[node])
+        if (x != parent)
+            dfs(x, node, temp);
+}
+
+int main()
+{
+    fastio;
+#ifdef LOCAL_OUTPUT
+    freopen(LOCAL_OUTPUT, "w", stdout);
+#endif
+#ifdef LOCAL_INPUT
+    freopen(LOCAL_INPUT, "r", stdin);
+#endif
+    cin >> n;
+    cost = vl(n);
+    adj = vvi(n);
+    rep(i, n) cin >> cost[i];
+    rep(i, n - 1)
+    {
+        int x, y;
+        cin >> x >> y;
+        x--;
+        y--;
+        adj[x].pb(y);
+        adj[y].pb(x);
+    }
+    dfs(0, -1, mli());
+    cout << ans << endl;
+}
