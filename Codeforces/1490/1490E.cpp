770003fe91085708db1c8902ab7a0224a411c48d
mwashief <muwashief@gmail.com>
2021-02-17 11:11:55 +0600

    Codeforces 1490E

---
+++ Codeforces/1490/1490E.cpp
@@ -0,0 +1,121 @@
+/*
+    Washief Hossain Mugdho
+    16 February 2021
+    1490 1490E
+*/
+
+#ifndef DEBUG
+#pragma GCC optimize("O2")
+#endif
+
+#include <bits/stdc++.h>
+#define pb push_back
+#define mp make_pair
+#define fr first
+#define sc second
+#define fastio ios_base::sync_with_stdio(0)
+#define untie cin.tie(0)
+#define rep(i, n) for (int i = 0; i < n; i++)
+#define repe(i, n) for (int i = 1; i <= n; i++)
+#define rrep(i, n) for (int i = n - 1; i >= 0; i--)
+#define rrepe(i, n) for (int i = n; i > 0; i--)
+#define ms(a, b) memset(a, b, sizeof a)
+#define MOD 1000000007
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+using namespace std;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vb = vector<bool>;
+using vi = vector<int>;
+using vl = vector<ll>;
+using vvb = vector<vector<bool>>;
+using vvi = vector<vector<int>>;
+using vvl = vector<vector<ll>>;
+using vpii = vector<pair<int, int>>;
+using mii = map<int, int>;
+
+/***********IO Utility**************/
+template <typename... ArgTypes>
+void print(ArgTypes... args);
+template <typename... ArgTypes>
+void input(ArgTypes &...args);
+template <>
+void print() {}
+template <>
+void input() {}
+template <typename T, typename... ArgTypes>
+void print(T t, ArgTypes... args)
+{
+    cout << t;
+    print(args...);
+}
+template <typename T, typename... ArgTypes>
+void input(T &t, ArgTypes &...args)
+{
+    cin >> t;
+    input(args...);
+}
+int n;
+vpii a;
+
+bool check(int index)
+{
+    ll tokens = 0;
+    for (int i = 0; i <= index; i++)
+        tokens += (ll)a[i].fr;
+    for (int i = index + 1; i < n; i++)
+    {
+        if (tokens < a[i].fr)
+            return 0;
+        tokens += (ll)a[i].fr;
+    }
+    return 1;
+}
+
+inline void _()
+{
+    cin >> n;
+    a.clear();
+    repe(i, n)
+    {
+        int x;
+        cin >> x;
+        a.pb({x, i});
+    }
+    sort(a.begin(), a.end());
+    int l = 0, r = n - 1;
+    while (l < r)
+    {
+        int mid = (l + r) >> 1;
+        if (check(mid))
+            r = mid;
+        else
+            l = mid + 1;
+    }
+    set<int> res;
+    for (int i = l; i < n; i++)
+        res.insert(a[i].sc);
+    cout << res.size() << endl;
+    for (auto winner : res)
+        cout << winner << " ";
+    cout << endl;
+}
+
+int main()
+{
+    fastio;
+#ifdef LOCAL_OUTPUT
+    freopen(LOCAL_OUTPUT, "w", stdout);
+#endif
+#ifdef LOCAL_INPUT
+    freopen(LOCAL_INPUT, "r", stdin);
+#endif
+
+    int __;
+    cin >> __;
+    while (__--)
+        _();
+}
