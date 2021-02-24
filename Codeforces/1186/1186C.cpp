63f86ad1af505193f7953ee87ed337934e927660
mwashief <muwashief@gmail.com>
2021-02-24 17:06:18 +0600

    Codeforces 1186C

---
+++ Codeforces/1186/1186C.cpp
@@ -0,0 +1,88 @@
+/*
+    Washief Hossain Mugdho
+    24 February 2021
+    1186 1186C
+*/
+
+#ifndef DEBUG
+#pragma GCC optimize("O2")
+#endif
+
+#include <bits/stdc++.h>
+#define pb push_back
+#define mp make_pair
+#define fr first
+#define sc second
+#define fastio ios_base::sync_with_stdio(0)
+#define untie cin.tie(0)
+#define rep(i, n) for (int i = 0; i < n; i++)
+#define repe(i, n) for (int i = 1; i <= n; i++)
+#define rrep(i, n) for (int i = n - 1; i >= 0; i--)
+#define rrepe(i, n) for (int i = n; i > 0; i--)
+#define ms(a, b) memset(a, b, sizeof a)
+#define MOD 1000000007
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+using namespace std;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vb = vector<bool>;
+using vi = vector<int>;
+using vl = vector<ll>;
+using vvb = vector<vector<bool>>;
+using vvi = vector<vector<int>>;
+using vvl = vector<vector<ll>>;
+using vpii = vector<pair<int, int>>;
+using mii = map<int, int>;
+
+/***********IO Utility**************/
+template <typename... ArgTypes>
+void print(ArgTypes... args);
+template <typename... ArgTypes>
+void input(ArgTypes &...args);
+template <>
+void print() {}
+template <>
+void input() {}
+template <typename T, typename... ArgTypes>
+void print(T t, ArgTypes... args)
+{
+    cout << t;
+    print(args...);
+}
+template <typename T, typename... ArgTypes>
+void input(T &t, ArgTypes &...args)
+{
+    cin >> t;
+    input(args...);
+}
+
+int main()
+{
+    fastio;
+#ifdef LOCAL_OUTPUT
+    freopen(LOCAL_OUTPUT, "w", stdout);
+#endif
+#ifdef LOCAL_INPUT
+    freopen(LOCAL_INPUT, "r", stdin);
+#endif
+    string s, t;
+    cin >> s >> t;
+    int one = 0;
+    for (char c : t)
+        if (c == '1')
+            one++;
+    rep(i, t.size()) if (s[i] == '1') one++;
+    int res = 1 - (int)one & 1;
+    for (int i = 1; i + t.size() <= s.size(); i++)
+    {
+        if (s[i + t.size() - 1] == '1')
+            one++;
+        if (s[i - 1] == '1')
+            one--;
+        res += 1 - (int)one & 1;
+    }
+    cout << res << endl;
+}
