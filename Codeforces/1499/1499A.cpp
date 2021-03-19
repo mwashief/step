d584c5752517f39f67596184dda4f96684a3bced
mwashief <muwashief@gmail.com>
2021-03-19 16:07:55 +0600

    Codeforces 1499A

---
+++ Codeforces/1499/1499A.cpp
@@ -0,0 +1,94 @@
+/*
+    Washief Hossain Mugdho
+    18 March 2021
+    Codeforces 1499 1499A
+*/
+
+#ifndef DEBUG
+#pragma GCC optimize("O2")
+#endif
+
+#include <bits/stdc++.h>
+#define pb push_back
+#define mp make_pair
+#define fr first
+#define sc second
+#define fastio ios_base::sync_with_stdio(0)
+#define untie cin.tie(0)
+#define rep(i, n) for (int i = 0; i < n; i++)
+#define repe(i, n) for (int i = 1; i <= n; i++)
+#define rrep(i, n) for (int i = n - 1; i >= 0; i--)
+#define rrepe(i, n) for (int i = n; i > 0; i--)
+#define ms(a, b) memset(a, b, sizeof a)
+#define MOD 1000000007
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+using namespace std;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vb = vector<bool>;
+using vi = vector<int>;
+using vl = vector<ll>;
+using vvb = vector<vector<bool>>;
+using vvi = vector<vector<int>>;
+using vvl = vector<vector<ll>>;
+using vpii = vector<pair<int, int>>;
+using mii = map<int, int>;
+
+/***********IO Utility**************/
+template <typename... ArgTypes>
+void print(ArgTypes... args);
+template <typename... ArgTypes>
+void input(ArgTypes &...args);
+template <>
+void print() {}
+template <>
+void input() {}
+template <typename T, typename... ArgTypes>
+void print(T t, ArgTypes... args)
+{
+    cout << t;
+    print(args...);
+}
+template <typename T, typename... ArgTypes>
+void input(T &t, ArgTypes &...args)
+{
+    cin >> t;
+    input(args...);
+}
+
+bool check(int k1, int k2, int p)
+{
+    int m = min(k1, k2);
+    int n = max(k1, k2) - m;
+    int tot = m + n / 2;
+    return tot >= p;
+}
+
+inline void _()
+{
+    int n, k1, k2, w, b;
+    cin >> n >> k1 >> k2 >> w >> b;
+    if (check(k1, k2, w) && check(n - k1, n - k2, b))
+        cout << "YES";
+    else
+        cout << "NO";
+    cout << endl;
+}
+
+int main()
+{
+    fastio;
+#ifdef LOCAL_OUTPUT
+    freopen(LOCAL_OUTPUT, "w", stdout);
+#endif
+#ifdef LOCAL_INPUT
+    freopen(LOCAL_INPUT, "r", stdin);
+#endif
+    int __;
+    cin >> __;
+    while (__--)
+        _();
+}
