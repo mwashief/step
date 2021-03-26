0cbe66c589c753c88786e203104665acab510503
mwashief <muwashief@gmail.com>
2021-03-26 11:02:29 +0600

    Codeforces 1506G

---
+++ Codeforces/1506/1506G.cpp
@@ -0,0 +1,115 @@
+/*
+    Washief Hossain Mugdho
+    25 March 2021
+    Codeforces 1506 1506G
+*/
+
+#ifndef DEBUG
+#pragma GCC optimize("O2")
+#endif
+
+#include <bits/stdc++.h>
+#define pb push_back
+#define mp make_pair
+#define fr first
+#define sc second
+#define fastio ios_base::sync_with_stdio(0)
+#define untie cin.tie(0)
+#define rep(i, n) for (int i = 0; i < n; i++)
+#define repe(i, n) for (int i = 1; i <= n; i++)
+#define rrep(i, n) for (int i = n - 1; i >= 0; i--)
+#define rrepe(i, n) for (int i = n; i > 0; i--)
+#define ms(a, b) memset(a, b, sizeof a)
+#define MOD 1000000007
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+using namespace std;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vb = vector<bool>;
+using vi = vector<int>;
+using vl = vector<ll>;
+using vvb = vector<vector<bool>>;
+using vvi = vector<vector<int>>;
+using vvl = vector<vector<ll>>;
+using vpii = vector<pair<int, int>>;
+using mii = map<int, int>;
+
+/***********IO Utility**************/
+template <typename... ArgTypes>
+void print(ArgTypes... args);
+template <typename... ArgTypes>
+void input(ArgTypes &...args);
+template <>
+void print() {}
+template <>
+void input() {}
+template <typename T, typename... ArgTypes>
+void print(T t, ArgTypes... args)
+{
+    cout << t;
+    print(args...);
+}
+template <typename T, typename... ArgTypes>
+void input(T &t, ArgTypes &...args)
+{
+    cin >> t;
+    input(args...);
+}
+
+inline void _()
+{
+    string s;
+    cin >> s;
+    vvi track(s.size(), vi(26, s.size()));
+    vi nex(26, s.size());
+    vb taken(26);
+    set<int> letters;
+    for (int i = s.size() - 1; i >= 0; i--)
+    {
+        int zz = s[i] - 'a';
+        letters.insert(zz);
+        rep(j, 26) track[i][j] = nex[j];
+        nex[zz] = i;
+    }
+    int i = 0;
+    rep(t, letters.size())
+    {
+        int maxLetter = -1;
+        int ind = -1;
+        for (int j = i; j < s.size(); j++)
+        {
+            int z = s[j] - 'a';
+            if (taken[z])
+                continue;
+            if (z > maxLetter)
+            {
+                maxLetter = z;
+                ind = j;
+            }
+            if (track[j][z] == s.size())
+                break;
+        }
+        i = ind + 1;
+        taken[maxLetter] = 1;
+        cout << char('a' + maxLetter);
+    }
+    cout << endl;
+}
+
+int main()
+{
+    fastio;
+#ifdef LOCAL_OUTPUT
+    freopen(LOCAL_OUTPUT, "w", stdout);
+#endif
+#ifdef LOCAL_INPUT
+    freopen(LOCAL_INPUT, "r", stdin);
+#endif
+    int __;
+    cin >> __;
+    while (__--)
+        _();
+}
