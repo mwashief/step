71dc9774096feaf354aa386fd9bae8b66c618bb5
mwashief <muwashief@gmail.com>
2021-03-26 11:01:58 +0600

    Codeforces 1506B

---
+++ Codeforces/1506/1506B.cpp
@@ -0,0 +1,116 @@
+/*
+    Washief Hossain Mugdho
+    25 March 2021
+    Codeforces 1506 1506B
+*/
+
+#ifndef DEBUG
+#pragma GCC optimize("O2")
+#endif
+
+#include <bits/stdc++.h>
+#define pb push_back
+#define mp make_pair
+#define fr first
+#define sc second
+#define fastio ios_base::sync_with_stdio(0)
+#define untie cin.tie(0)
+#define rep(i, n) for (int i = 0; i < n; i++)
+#define repe(i, n) for (int i = 1; i <= n; i++)
+#define rrep(i, n) for (int i = n - 1; i >= 0; i--)
+#define rrepe(i, n) for (int i = n; i > 0; i--)
+#define ms(a, b) memset(a, b, sizeof a)
+#define MOD 1000000007
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+using namespace std;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vb = vector<bool>;
+using vi = vector<int>;
+using vl = vector<ll>;
+using vvb = vector<vector<bool>>;
+using vvi = vector<vector<int>>;
+using vvl = vector<vector<ll>>;
+using vpii = vector<pair<int, int>>;
+using mii = map<int, int>;
+
+/***********IO Utility**************/
+template <typename... ArgTypes>
+void print(ArgTypes... args);
+template <typename... ArgTypes>
+void input(ArgTypes &...args);
+template <>
+void print() {}
+template <>
+void input() {}
+template <typename T, typename... ArgTypes>
+void print(T t, ArgTypes... args)
+{
+    cout << t;
+    print(args...);
+}
+template <typename T, typename... ArgTypes>
+void input(T &t, ArgTypes &...args)
+{
+    cin >> t;
+    input(args...);
+}
+
+inline void _()
+{
+    int n, k;
+    cin >> n >> k;
+    string s;
+    cin >> s;
+    vi nex(n);
+    nex[n - 1] = n;
+    for (int i = n - 2; i >= 0; i--)
+    {
+        if (s[i + 1] == '*')
+            nex[i] = i + 1;
+        else
+            nex[i] = nex[i + 1];
+    }
+    int last = -1;
+    int res = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (s[i] == '*')
+        {
+            if (last == -1)
+            {
+                res++;
+                last = i;
+            }
+            else if (nex[i] == n)
+            {
+                res++;
+                break;
+            }
+            else if (nex[i] - last > k)
+            {
+                res++;
+                last = i;
+            }
+        }
+    }
+    cout << res << endl;
+}
+
+int main()
+{
+    fastio;
+#ifdef LOCAL_OUTPUT
+    freopen(LOCAL_OUTPUT, "w", stdout);
+#endif
+#ifdef LOCAL_INPUT
+    freopen(LOCAL_INPUT, "r", stdin);
+#endif
+    int __;
+    cin >> __;
+    while (__--)
+        _();
+}
