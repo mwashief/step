16db6c5f27fa61bb90f1ce173e20eb4aaca6c9ce
mwashief <muwashief@gmail.com>
2021-04-09 12:05:33 +0600

    Codeforces 1495C

---
+++ Codeforces/1495/1495C.cpp
@@ -0,0 +1,160 @@
+/*
+    Washief Hossain Mugdho
+    09 April 2021
+    Codeforces 1495 1495C
+*/
+
+#ifndef DEBUG
+#pragma GCC optimize("O2")
+#endif
+
+#include <bits/stdc++.h>
+#define pb push_back
+#define mp make_pair
+#define fr first
+#define sc second
+#define fastio ios_base::sync_with_stdio(0)
+#define untie cin.tie(0)
+#define rep(i, n) for (int i = 0; i < n; i++)
+#define repe(i, n) for (int i = 1; i <= n; i++)
+#define rrep(i, n) for (int i = n - 1; i >= 0; i--)
+#define rrepe(i, n) for (int i = n; i > 0; i--)
+#define ms(a, b) memset(a, b, sizeof a)
+#define MOD 1000000007
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+using namespace std;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vb = vector<bool>;
+using vi = vector<int>;
+using vc = vector<char>;
+using vl = vector<ll>;
+using vvb = vector<vector<bool>>;
+using vvi = vector<vector<int>>;
+using vvc = vector<vector<char>>;
+using vvl = vector<vector<ll>>;
+using vpii = vector<pair<int, int>>;
+using mii = map<int, int>;
+using umii = unordered_map<int, int>;
+using seti = set<int>;
+using useti = unordered_set<int>;
+
+/***********IO Utility**************/
+template <typename... ArgTypes>
+void print(ArgTypes... args);
+template <typename... ArgTypes>
+void input(ArgTypes &...args);
+template <>
+void print() {}
+template <>
+void input() {}
+template <typename T, typename... ArgTypes>
+void print(T t, ArgTypes... args)
+{
+    cout << t;
+    print(args...);
+}
+template <typename T, typename... ArgTypes>
+void input(T &t, ArgTypes &...args)
+{
+    cin >> t;
+    input(args...);
+}
+int n, m;
+vvc mat;
+vi r;
+
+void traverse(int a, int state)
+{
+    if (a >= n)
+        return;
+    if (state == 0)
+    {
+        rep(i, m) mat[a][i] = 'X';
+        if (a == n - 1)
+            return;
+        int ind = 0;
+        if (r[a + 1])
+        {
+            if (r[a + 2])
+                return traverse(a + 2, 1);
+            rep(i, m) if (mat[a + 1][i] == 'X')
+            {
+                ind = i;
+                break;
+            }
+        }
+        for (int i = a + 2; i < n; i++)
+            if (r[i])
+            {
+                for (int j = a + 1; j <= i; j++)
+                    mat[j][ind] = 'X';
+                return traverse(i, 0);
+            }
+        return;
+    }
+    int nn = 0;
+    for (int i = a + 1; i < n; i++)
+        if (r[i])
+        {
+            nn = i;
+            break;
+        }
+
+    if (nn)
+    {
+        rep(i, m) if (mat[a][i] == 'X')
+        {
+            for (int j = a; j <= nn; j++)
+                mat[j][i] = 'X';
+        }
+        int ind = -1;
+        rep(i, m) if (mat[a - 1][i] == 'X') ind = i;
+        for (int j = a; j <= nn; j++)
+            mat[j][ind] = 'X';
+        return traverse(nn, 0);
+    }
+    rep(i, m) if (mat[a][i] == 'X') mat[a - 1][i] = 'X';
+}
+
+inline void _()
+{
+    cin >> n >> m;
+    mat = vvc(n, vc(m));
+    r = vi(n + 1);
+    rep(i, n) rep(j, m)
+    {
+        cin >> mat[i][j];
+        if (mat[i][j] == 'X')
+            r[i]++;
+    }
+    rep(i, n) if (r[i])
+    {
+        traverse(i, 0);
+        break;
+    }
+    rep(i, n)
+    {
+        rep(j, m) cout << mat[i][j];
+        cout << endl;
+    }
+    cout << endl;
+}
+
+int main()
+{
+    fastio;
+#ifdef LOCAL_OUTPUT
+    freopen(LOCAL_OUTPUT, "w", stdout);
+#endif
+#ifdef LOCAL_INPUT
+    freopen(LOCAL_INPUT, "r", stdin);
+#endif
+    int __;
+    cin >> __;
+    while (__--)
+        _();
+}
