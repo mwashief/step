7db485ecc1026f279dcd81aeeae8ac90dc5c369d
mwashief <muwashief@gmail.com>
2021-04-11 17:27:45 +0600

    Codeforces 1512E

---
+++ Codeforces/1512/1512E.cpp
@@ -0,0 +1,131 @@
+/*
+    Washief Hossain Mugdho
+    10 April 2021
+    Codeforces 1512 1512E
+*/
+
+#ifndef DEBUG
+#pragma GCC optimize("O2")
+#endif
+
+#include <bits/stdc++.h>
+#define pb push_back
+#define mp make_pair
+#define fr first
+#define sc second
+#define fastio ios_base::sync_with_stdio(0)
+#define untie cin.tie(0)
+#define rep(i, n) for (int i = 0; i < n; i++)
+#define repe(i, n) for (int i = 1; i <= n; i++)
+#define rrep(i, n) for (int i = n - 1; i >= 0; i--)
+#define rrepe(i, n) for (int i = n; i > 0; i--)
+#define ms(a, b) memset(a, b, sizeof a)
+#define MOD 1000000007
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+using namespace std;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vb = vector<bool>;
+using vi = vector<int>;
+using vl = vector<ll>;
+using vvb = vector<vector<bool>>;
+using vvi = vector<vector<int>>;
+using vvl = vector<vector<ll>>;
+using vpii = vector<pair<int, int>>;
+using mii = map<int, int>;
+using umii = unordered_map<int, int>;
+using seti = set<int>;
+using useti = unordered_set<int>;
+
+/***********IO Utility**************/
+template <typename... ArgTypes>
+void print(ArgTypes... args);
+template <typename... ArgTypes>
+void input(ArgTypes &...args);
+template <>
+void print() {}
+template <>
+void input() {}
+template <typename T, typename... ArgTypes>
+void print(T t, ArgTypes... args)
+{
+    cout << t;
+    print(args...);
+}
+template <typename T, typename... ArgTypes>
+void input(T &t, ArgTypes &...args)
+{
+    cin >> t;
+    input(args...);
+}
+
+inline void _()
+{
+    int n, l, r, s;
+    cin >> n >> l >> r >> s;
+    l--;
+    r--;
+    int now = l;
+    vi ans(n);
+    useti all;
+    repe(i, n) all.insert(i);
+    for (int i = l; i <= r; i++)
+    {
+        ans[i] = i - l + 1;
+        s -= i - l + 1;
+    }
+    if (s < 0)
+    {
+        cout << -1 << endl;
+        return;
+    }
+    int nums = r - l + 1;
+    int each = s / nums;
+    int extra = s % nums;
+    for (int i = l; i <= r; i++)
+    {
+        ans[i] += each;
+    }
+    int x = r;
+    while (extra)
+    {
+        ans[x]++;
+        x--;
+        extra--;
+    }
+    for (int i = l; i <= r; i++)
+    {
+        if (ans[i] < 1 || ans[i] > n)
+        {
+            cout << -1 << endl;
+            return;
+        }
+        all.erase(ans[i]);
+    }
+    rep(i, n) if (ans[i] == 0)
+    {
+        ans[i] = *all.begin();
+        all.erase(ans[i]);
+    }
+    rep(i, n) cout << ans[i] << " ";
+    cout << endl;
+}
+
+int main()
+{
+    fastio;
+#ifdef LOCAL_OUTPUT
+    freopen(LOCAL_OUTPUT, "w", stdout);
+#endif
+#ifdef LOCAL_INPUT
+    freopen(LOCAL_INPUT, "r", stdin);
+#endif
+
+    int __;
+    cin >> __;
+    while (__--)
+        _();
+}
