0807922aa5798d9730aae5ca2066fa93459cef66
mwashief <muwashief@gmail.com>
2021-04-28 17:03:06 +0600

    compiler assistant updated

--- CppCompile.java
+++ CppCompile.java
@@ -1,3 +1,5 @@
+import java.io.File;
+
 public class CppCompile {
     public static final String ANSI_RESET = "\u001B[0m";
     public static final String ANSI_BLACK = "\u001B[30m";
@@ -48,16 +50,23 @@ public class CppCompile {
                     "-DLOCAL_OUTPUT=\"" + System.getProperty("user.dir") + "/IO/output.dat\"",
                     "-DLOCAL_INPUT=\"" + System.getProperty("user.dir") + "/IO/input.dat\"",
                     path + "/" + file + "." + extension, "-o", path + "/" + file + ".out" };
-
+        
+            new File(path + "/" + file + ".out").delete();
             new ProcessBuilder(fargs).inheritIO().start().waitFor();
-            System.out.println(ANSI_GREEN + "\u2713" + ANSI_RESET + ANSI_YELLOW + " Compilation done" + ANSI_RESET);
-            long startTime = System.nanoTime();
-            new ProcessBuilder(new String[] { path + "/" + file + ".out" }).inheritIO().start().waitFor();
-            long endTime = System.nanoTime();
-            long timeElapsed = endTime - startTime;
-            System.out.println("\u2713 Execution done");
-            System.out.println(ANSI_RED + "Execution time: " + ANSI_RED_BACKGROUND + ANSI_BLACK + timeElapsed / 1000000
-                    + "ms" + ANSI_RESET);
+            System.out.println(ANSI_GREEN + "\u2705" + ANSI_RESET + ANSI_YELLOW + " Compilation process done" + ANSI_RESET);
+            try{
+                long startTime = System.nanoTime();
+                new ProcessBuilder(new String[] { path + "/" + file + ".out" }).inheritIO().start().waitFor();
+                long endTime = System.nanoTime();
+                long timeElapsed = endTime - startTime;
+                System.out.println("\u2705 Execution done");
+                System.out.println(ANSI_RED + "-- Execution time: " + ANSI_RED_BACKGROUND + ANSI_BLACK + timeElapsed / 1000000
+                        + "ms" + ANSI_RESET);
+            }
+            catch(Exception e)
+            {
+                System.out.println(ANSI_RED + "\u274c Compilation unsuccessful" + ANSI_RESET);
+            }
         } else {
             System.out.println(ANSI_PURPLE + "OS: " + ANSI_RESET + ANSI_BLUE + "Windows" + ANSI_RESET);
             System.out.println(ANSI_PURPLE + "Path: " + ANSI_RESET + ANSI_BLUE + path + ANSI_RESET);
@@ -82,18 +91,23 @@ public class CppCompile {
                     "-DLOCAL_OUTPUT=\\\"" + dir + "\\\\IO\\\\output.dat\\\"",
                     "-DLOCAL_INPUT=\\\"" + dir + "\\\\IO\\\\input.dat\\\"",
                     "\"" + path + "\\" + file + "." + extension + "\"", "-o", "\"" + path + "\\" + file + "\"" };
-
+            new File(path + "\\" + file + ".exe").delete();
             new ProcessBuilder(fargs).inheritIO().start().waitFor();
-            System.out.println(ANSI_GREEN + "\u2713" + ANSI_RESET + ANSI_YELLOW + " Compilation done" + ANSI_RESET);
-            long startTime = System.nanoTime();
-            new ProcessBuilder(new String[] { "cmd.exe", "/c", "\"" + path + "\\" + file + "\"" }).inheritIO().start()
-                    .waitFor();
-            long endTime = System.nanoTime();
-            long timeElapsed = endTime - startTime;
-            System.out.println("\u2713 Execution done");
-            System.out.println(ANSI_RED + "Execution time: " + ANSI_RED_BACKGROUND + ANSI_BLACK + timeElapsed / 1000000
-                    + "ms" + ANSI_RESET);
-
+            System.out.println(ANSI_GREEN + "\u2705" + ANSI_RESET + ANSI_YELLOW + " Compilation process done" + ANSI_RESET);
+            try{
+                long startTime = System.nanoTime();
+                new ProcessBuilder(new String[] { "cmd.exe", "/c", "\"" + path + "\\" + file + "\"" }).inheritIO().start()
+                        .waitFor();
+                long endTime = System.nanoTime();
+                long timeElapsed = endTime - startTime;
+                System.out.println("\u2705 Execution done");
+                System.out.println(ANSI_RED + "-- Execution time: " + ANSI_RED_BACKGROUND + ANSI_BLACK + timeElapsed / 1000000
+                        + "ms" + ANSI_RESET);
+            }
+            catch(Exception e)
+            {
+                System.out.println(ANSI_RED + "\u274c Compilation unsuccessful" + ANSI_RESET);
+            }
         }
 
     }
