01ee38bc5af5d99cc6798ac4101e856c5286f3d9
mwashief <muwashief@gmail.com>
2021-05-03 22:57:21 +0600

    optional code eliminated

--- Templates/FST.cpp
+++ Templates/FST.cpp
@@ -4,60 +4,12 @@
     Algorithm FST
 */
 
-#ifndef DEBUG
-#pragma GCC optimize("O2")
-#endif
-
 #include <bits/stdc++.h>
-#define pb push_back
-#define mp make_pair
-#define fr first
-#define sc second
-#define fastio ios_base::sync_with_stdio(0)
-#define untie cin.tie(0)
-#define rep(i, n) for (int i = 0; i < n; i++)
-#define repe(i, n) for (int i = 1; i <= n; i++)
-#define rrep(i, n) for (int i = n - 1; i >= 0; i--)
-#define rrepe(i, n) for (int i = n; i > 0; i--)
-#define ms(a, b) memset(a, b, sizeof a)
-#define MOD 1000000007
 
 typedef long long ll;
 typedef unsigned long long ull;
 
 using namespace std;
-using pii = pair<int, int>;
-using pll = pair<ll, ll>;
-using vb = vector<bool>;
-using vi = vector<int>;
-using vl = vector<ll>;
-using vvb = vector<vector<bool>>;
-using vvi = vector<vector<int>>;
-using vvl = vector<vector<ll>>;
-using vpii = vector<pair<int, int>>;
-using mii = map<int, int>;
-
-/***********IO Utility**************/
-template <typename... ArgTypes>
-void print(ArgTypes... args);
-template <typename... ArgTypes>
-void input(ArgTypes &...args);
-template <>
-void print() {}
-template <>
-void input() {}
-template <typename T, typename... ArgTypes>
-void print(T t, ArgTypes... args)
-{
-    cout << t;
-    print(args...);
-}
-template <typename T, typename... ArgTypes>
-void input(T &t, ArgTypes &...args)
-{
-    cin >> t;
-    input(args...);
-}
 
 template <class T, class OP>
 class SegmentTree
@@ -143,10 +95,11 @@ public:
 
 int main()
 {
-    vi a = {54, 756, 674, 6, 26, 23, 625, 624, 56, 246, 254, 437, 47};
+    vector<int> a = {54, 756, 674, 6, 26, 23, 625, 624, 56, 246, 254, 437, 47};
     SegmentTree<int, Add<int>> ss(a);
-    rep(i, a.size()) for (int j = i + 1; j < a.size(); j++)
-    {
-        cout << i << ", " << j << ": " << ss.query(i, j) << endl;
-    }
+    for (int i = 0; i < a.size(); i++)
+        for (int j = i + 1; j < a.size(); j++)
+        {
+            cout << i << ", " << j << ": " << ss.query(i, j) << endl;
+        }
 }
