a46198afc69addcf9c8e5c740e84fa57c7f45ee0
mwashief <muwashief@gmail.com>
2021-05-11 13:42:00 +0600

    Codeforces 1291B

---
+++ Codeforces/1291/1291B.cpp
@@ -0,0 +1,93 @@
+/*
+    Washief Hossain Mugdho
+    11 May 2021
+    Codeforces 1291 1291B
+*/
+
+#ifndef DEBUG
+#pragma GCC optimize("Ofast")
+#pragma GCC optimization("unroll-loops, no-stack-protector")
+#pragma GCC target("avx,avx2,fma")
+#endif
+
+#include <bits/stdc++.h>
+#define pb push_back
+#define mp make_pair
+#define fr first
+#define sc second
+#define pyes cout << "Yes"
+#define pno cout << "No"
+#define fastio ios_base::sync_with_stdio(0)
+#define untie cin.tie(0)
+#define rep(i, n) for (int i = 0; i < (n); (i)++)
+#define repe(i, n) for (int i = 1; i <= (n); (i)++)
+#define rrep(i, n) for (int i = (n)-1; i >= 0; (i)--)
+#define rrepe(i, n) for (int i = (n); i > 0; (i)--)
+#define ms(a, b) memset(a, b, sizeof a)
+#define a2z(x) (x).begin(), (x).end()
+#define z2a(x) (x).rbegin(), (x).rend()
+#define endl "\n"
+#define gsz(v) ((int)(v).size())
+#define MOD 1000000007
+#define MAX 1000005
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+using namespace std;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vb = vector<bool>;
+using vi = vector<int>;
+using vl = vector<ll>;
+using vvb = vector<vector<bool>>;
+using vvi = vector<vector<int>>;
+using vvl = vector<vector<ll>>;
+using vpii = vector<pair<int, int>>;
+using mii = map<int, int>;
+using umii = unordered_map<int, int>;
+using seti = set<int>;
+using useti = unordered_set<int>;
+
+inline void _()
+{
+    int n;
+    cin >> n;
+    vi a(n);
+    rep(i, n) cin >> a[i];
+    vb prefix(n), suffix(n);
+    rep(i, n)
+    {
+        if (a[i] < i)
+            break;
+        prefix[i] = 1;
+    }
+    rrep(i, n)
+    {
+        if (a[i] < n - 1 - i)
+            break;
+        suffix[i] = 1;
+    }
+    rep(i, n) if (prefix[i] && suffix[i])
+    {
+        pyes << endl;
+        return;
+    }
+    pno << endl;
+}
+
+int main()
+{
+    fastio;
+#ifdef LOCAL_OUTPUT
+    freopen(LOCAL_OUTPUT, "w", stdout);
+#endif
+#ifdef LOCAL_INPUT
+    freopen(LOCAL_INPUT, "r", stdin);
+#endif
+
+    int __;
+    cin >> __;
+    while (__--)
+        _();
+}
