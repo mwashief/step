545ce6aaf2c44eb86f365220bbd36c3aa7f91016
mwashief <muwashief@gmail.com>
2021-05-12 11:26:55 +0600

    Codeforces 1545D

---
+++ Codeforces/1445/1445D.cpp
@@ -0,0 +1,93 @@
+/*
+    Washief Hossain Mugdho
+    12 May 2021
+    Codeforces 1445 1445D
+*/
+
+#ifndef DEBUG
+#pragma GCC optimize("Ofast")
+#pragma GCC optimization("unroll-loops, no-stack-protector")
+#pragma GCC target("avx,avx2,fma")
+#endif
+
+#include <bits/stdc++.h>
+#define pb push_back
+#define mp make_pair
+#define fr first
+#define sc second
+#define pyes cout << "YES"
+#define pno cout << "NO"
+#define fastio ios_base::sync_with_stdio(0)
+#define untie cin.tie(0)
+#define rep(i, n) for (int i = 0; i < (n); (i)++)
+#define repe(i, n) for (int i = 1; i <= (n); (i)++)
+#define rrep(i, n) for (int i = (n)-1; i >= 0; (i)--)
+#define rrepe(i, n) for (int i = (n); i > 0; (i)--)
+#define ms(a, b) memset(a, b, sizeof a)
+#define a2z(x) (x).begin(), (x).end()
+#define z2a(x) (x).rbegin(), (x).rend()
+#define endl "\n"
+#define gsz(v) ((int)(v).size())
+#define MOD 998244353
+#define MAX 1000005
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+using namespace std;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vb = vector<bool>;
+using vi = vector<int>;
+using vl = vector<ll>;
+using vvb = vector<vector<bool>>;
+using vvi = vector<vector<int>>;
+using vvl = vector<vector<ll>>;
+using vpii = vector<pair<int, int>>;
+using mii = map<int, int>;
+using umii = unordered_map<int, int>;
+using seti = set<int>;
+using useti = unordered_set<int>;
+
+int modPow(ll a, ll b)
+{
+    ll res = 1;
+    while (b)
+    {
+        if (b & 1)
+            res = res * a % MOD;
+        a = a * a % MOD;
+        b >>= 1;
+    }
+    return res;
+}
+
+int main()
+{
+    fastio;
+#ifdef LOCAL_OUTPUT
+    freopen(LOCAL_OUTPUT, "w", stdout);
+#endif
+#ifdef LOCAL_INPUT
+    freopen(LOCAL_INPUT, "r", stdin);
+#endif
+
+    int n;
+    cin >> n;
+    vi a(n << 1);
+    rep(i, n << 1) cin >> a[i];
+    sort(a2z(a));
+    ll ans = 0;
+    rep(i, n) ans -= a[i];
+    for (int i = n; i < (n << 1); i++)
+        ans += a[i];
+    ans = ans % MOD;
+    ll p = 1, q = 1;
+    for (int i = (n << 1); i > n; i--)
+    {
+        p = p * i % MOD;
+        q = q * ((n << 1) + 1LL - i) % MOD;
+    }
+    p = p * modPow(q, MOD - 2) % MOD;
+    cout << p * ans % MOD;
+}
