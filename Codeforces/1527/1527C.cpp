e7560fac0271f12e2a584bdc4dc19648c6fbfd66
mwashief <muwashief@gmail.com>
2021-05-21 20:13:20 +0600

    Codeforces 1527C

---
+++ Codeforces/1527/1527C.cpp
@@ -0,0 +1,102 @@
+/*
+    Washief Hossain Mugdho
+    21 May 2021
+    Codeforces 1527 1527C
+*/
+
+#ifndef DEBUG
+#pragma GCC optimize("Ofast")
+#pragma GCC optimization("unroll-loops, no-stack-protector")
+#pragma GCC target("avx,avx2,fma")
+#endif
+
+#undef __INTERACTIVE__
+
+#include <bits/stdc++.h>
+#define pb push_back
+#define mp make_pair
+#define fr first
+#define sc second
+#define pyes cout << "YES" //"Yes"
+#define pno cout << "NO"   //"No"
+#define fastio ios_base::sync_with_stdio(0)
+#define untie cin.tie(0)
+#define rep(i, n) for (int i = 0; i < (n); (i)++)
+#define repe(i, n) for (int i = 1; i <= (n); (i)++)
+#define rrep(i, n) for (int i = (n)-1; i >= 0; (i)--)
+#define rrepe(i, n) for (int i = (n); i > 0; (i)--)
+#define ms(a, b) memset(a, b, sizeof a)
+#define a2z(x) (x).begin(), (x).end()
+#define z2a(x) (x).rbegin(), (x).rend()
+#define gsz(v) ((int)(v).size())
+#define MOD 1000000007 //998244353
+#define MAX 1000005
+
+#ifndef __INTERACTIVE__
+#define endl "\n"
+#endif
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+using vb = vector<bool>;
+using vi = vector<int>;
+using vl = vector<ll>;
+using vpii = vector<pair<int, int>>;
+
+using vvb = vector<vector<bool>>;
+using vvi = vector<vector<int>>;
+using vvl = vector<vector<ll>>;
+using vvpii = vector<vector<pair<int, int>>>;
+
+using mii = map<int, int>;
+using umii = unordered_map<int, int>;
+using seti = set<int>;
+using useti = unordered_set<int>;
+
+inline void __run_test()
+{
+    int n;
+    cin >> n;
+    map<int, vl> track;
+    ll res = 0LL;
+    repe(i, n)
+    {
+        int x;
+        cin >> x;
+        track[x].pb(i);
+    }
+    for (auto it = track.begin(); it != track.end(); it++)
+    {
+        sort(a2z(it->sc));
+        for (int i = it->sc.size() - 2; i >= 0; i--)
+        {
+            res += 1LL * it->sc[i] * (1LL * (it->sc.size() - i - 1) * (n + 1LL) - it->sc[i + 1]);
+            it->sc[i] += it->sc[i + 1];
+        }
+    }
+    cout << res << endl;
+}
+
+int main()
+{
+    fastio;
+#ifndef __INTERACTIVE__
+    untie;
+#endif
+#ifdef LOCAL_OUTPUT
+    freopen(LOCAL_OUTPUT, "w", stdout);
+#endif
+#ifdef LOCAL_INPUT
+    freopen(LOCAL_INPUT, "r", stdin);
+#endif
+    int __tests;
+    cin >> __tests;
+    while (__tests--)
+        __run_test();
+}
