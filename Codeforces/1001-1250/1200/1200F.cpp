6e910a0a6e7e6ca64556822616bc3d38016690c4
mwashief <muwashief@gmail.com>
2021-06-03 21:24:19 +0600

    Codeforces 1200F

---
+++ Codeforces/1001-1250/1200/1200F.cpp
@@ -0,0 +1,177 @@
+/*
+    Washief Hossain Mugdho
+    03 June 2021
+    Codeforces 1001-1250 1200 1200F
+*/
+
+#ifndef __DEBUG__
+#pragma GCC optimize("Ofast")
+#pragma GCC optimization("unroll-loops, no-stack-protector")
+#pragma GCC target("avx,avx2,fma")
+#endif
+
+#undef __INTERACTIVE__
+
+#include <bits/stdc++.h>
+#define pb push_back
+#define mp make_pair
+#define fr first
+#define sc second
+#define pyes cout << "YES" //"Yes"
+#define pno cout << "NO"   //"No"
+#define fastio ios_base::sync_with_stdio(0)
+#define untie cin.tie(0)
+#define rep(i, n) for (int i = 0; i < (n); (i)++)
+#define repe(i, n) for (int i = 1; i <= (n); (i)++)
+#define rrep(i, n) for (int i = (n)-1; i >= 0; (i)--)
+#define rrepe(i, n) for (int i = (n); i > 0; (i)--)
+#define ms(a, b) memset(a, b, sizeof a)
+#define a2z(x) (x).begin(), (x).end()
+#define z2a(x) (x).rbegin(), (x).rend()
+#define gsz(v) ((int)(v).size())
+#define MAX 1000005
+
+#if !defined(__INTERACTIVE__) && !defined(__DEBUG__)
+#define endl "\n"
+#endif
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+using vb = vector<bool>;
+using vi = vector<int>;
+using vl = vector<ll>;
+using vpii = vector<pair<int, int>>;
+
+using vvb = vector<vector<bool>>;
+using vvi = vector<vector<int>>;
+using vvl = vector<vector<ll>>;
+using vvpii = vector<vector<pair<int, int>>>;
+
+using mii = map<int, int>;
+using umii = unordered_map<int, int>;
+using seti = set<int>;
+using useti = unordered_set<int>;
+
+namespace std
+{
+
+    template <class Fun>
+    class y_combinator_result
+    {
+        Fun fun_;
+
+    public:
+        template <class T>
+        explicit y_combinator_result(T &&fun) : fun_(std::forward<T>(fun)) {}
+
+        template <class... Args>
+        decltype(auto) operator()(Args &&...args)
+        {
+            return fun_(std::ref(*this), std::forward<Args>(args)...);
+        }
+    };
+
+    template <class Fun>
+    decltype(auto) y_combinator(Fun &&fun)
+    {
+        return y_combinator_result<std::decay_t<Fun>>(std::forward<Fun>(fun));
+    }
+
+    template <class T>
+    T &unmove(T &&t) { return t; }
+
+}
+
+int main()
+{
+    fastio;
+#if !defined(__INTERACTIVE__) && !defined(__DEBUG__)
+    untie;
+#endif
+#ifdef LOCAL_OUTPUT
+    freopen(LOCAL_OUTPUT, "w", stdout);
+#endif
+#ifdef LOCAL_INPUT
+    freopen(LOCAL_INPUT, "r", stdin);
+#endif
+
+    int n, lcm = 1;
+    cin >> n;
+
+    vvi adj(n);
+    vi k(n);
+    rep(i, n) cin >> k[i];
+    rep(i, n)
+    {
+        int m;
+        cin >> m;
+        lcm = lcm * m / __gcd(lcm, m);
+        adj[i].resize(m);
+        rep(j, m)
+        {
+            cin >> adj[i][j];
+            adj[i][j]--;
+        }
+    }
+
+    int nodes = n * lcm;
+    vi visited(nodes), res(nodes);
+
+    auto getNext = [&](int node)
+    {
+        int name = node / lcm;
+        int state = node % lcm;
+        int edge = state % adj[name].size();
+        int nextNode = adj[name][edge];
+        int cost = state + k[nextNode];
+        cost %= lcm;
+        if (cost < 0)
+            cost += lcm;
+        return lcm * nextNode + cost;
+    };
+
+    auto traverse = y_combinator([&](auto self, int node, int starter) -> int
+                                 {
+                                     if (visited[node] == 0)
+                                         visited[node] = starter;
+                                     else if (visited[node] == starter)
+                                     {
+                                         useti s;
+                                         int now = node;
+                                         while (1)
+                                         {
+                                             s.insert(now / lcm);
+                                             now = getNext(now);
+                                             if (now == node)
+                                                 break;
+                                         }
+                                         return s.size();
+                                     }
+                                     else
+                                         return res[node];
+
+                                     return res[node] = self(getNext(node), starter);
+                                 });
+
+    rep(i, nodes) if (!visited[i]) traverse(i, i + 1);
+
+    int q;
+    cin >> q;
+    rep(i, q)
+    {
+        int x, y;
+        cin >> x >> y;
+        x--;
+        y += k[x];
+        y %= lcm;
+        if (y < 0)
+            y += lcm;
+        cout << res[x * lcm + y] << endl;
+    }
+}
