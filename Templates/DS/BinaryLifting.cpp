39091373b2d7a510cd78d9e37bb3a4a67b4c014d
mwashief <muwashief@gmail.com>
2021-06-06 13:16:23 +0600

    binary lifting implemented

---
+++ Templates/DS/BinaryLifting.cpp
@@ -0,0 +1,124 @@
+/*
+    Washief Hossain Mugdho
+    06 June 2021
+    Templates DS BinaryLifting
+*/
+
+#ifndef __DEBUG__
+#pragma GCC optimize("Ofast")
+#pragma GCC optimization("unroll-loops, no-stack-protector")
+#pragma GCC target("avx,avx2,fma")
+#endif
+
+#undef __INTERACTIVE__
+
+#include <bits/stdc++.h>
+#define pb push_back
+#define mp make_pair
+#define fr first
+#define sc second
+#define pyes cout << "YES" //"Yes"
+#define pno cout << "NO"   //"No"
+#define fastio ios_base::sync_with_stdio(0)
+#define untie cin.tie(0)
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define repe(i, n) for (int i = 1; i <= (n); i++)
+#define rrep(i, n) for (int i = (n)-1; i >= 0; i--)
+#define rrepe(i, n) for (int i = (n); i > 0; i--)
+#define ms(a, b) memset(a, b, sizeof a)
+#define a2z(x) (x).begin(), (x).end()
+#define z2a(x) (x).rbegin(), (x).rend()
+#define gsz(v) ((int)(v).size())
+#define MOD 1000000007 //998244353
+#define MAX 1000005
+
+#if !defined(__INTERACTIVE__) && !defined(__DEBUG__)
+#define endl "\n"
+#endif
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+using vb = vector<bool>;
+using vi = vector<int>;
+using vl = vector<ll>;
+using vpii = vector<pair<int, int>>;
+
+using vvb = vector<vector<bool>>;
+using vvi = vector<vector<int>>;
+using vvl = vector<vector<ll>>;
+using vvpii = vector<vector<pair<int, int>>>;
+
+using mii = map<int, int>;
+using umii = unordered_map<int, int>;
+using seti = set<int>;
+using useti = unordered_set<int>;
+
+class BinaryLifting
+{
+public:
+    static const int logHeight = 24;
+    std::vector<std::vector<int>> lift;
+    int n;
+    BinaryLifting(int numberOfElements) : n(numberOfElements)
+    {
+        lift = vector<vector<int>>(n, vector<int>(logHeight, -1));
+    }
+
+    void attach(int element, int parent)
+    {
+        lift[element][0] = parent;
+        for (int i = 1; i < logHeight; i++)
+        {
+            if (lift[element][i - 1] == -1)
+                lift[element][i] = -1;
+            else
+                lift[element][i] = lift[lift[element][i - 1]][i - 1];
+        }
+    }
+
+    void lift(int element)
+    {
+        for (int i = 1; i < logHeight; i++)
+        {
+            if (lift[element][i - 1] == -1)
+                lift[element][i] = -1;
+            else
+                lift[element][i] = lift[lift[element][i - 1]][i - 1];
+        }
+    }
+
+    int getPrevious(int element, int steps)
+    {
+        for (int i = logHeight - 1; i >= 0; i--)
+        {
+            if (element == -1)
+                return element;
+            if ((1 << i) <= steps)
+            {
+                element = lift[element][i];
+                steps -= (1 << i);
+            }
+        }
+        return element;
+    }
+};
+
+int main()
+{
+    fastio;
+#if !defined(__INTERACTIVE__) && !defined(__DEBUG__)
+    untie;
+#ifdef LOCAL_OUTPUT
+    freopen(LOCAL_OUTPUT, "w", stdout);
+#endif
+#ifdef LOCAL_INPUT
+    freopen(LOCAL_INPUT, "r", stdin);
+#endif
+#endif
+}
