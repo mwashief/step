ef5856c5c38eac82e75ef66092413a0626750ba2
mwashief <muwashief@gmail.com>
2021-06-11 21:53:56 +0600

    Codeforces 1538G

---
+++ Codeforces/1501-1750/1538/1538G.cpp
@@ -0,0 +1,115 @@
+/*
+    Washief Hossain Mugdho
+    11 June 2021
+    Codeforces 1501-1750 1538 1538G
+*/
+
+#ifndef __DEBUG__
+#pragma GCC optimize("Ofast")
+#pragma GCC optimization("unroll-loops, no-stack-protector")
+#pragma GCC target("avx,avx2,fma")
+#endif
+
+#undef __INTERACTIVE__
+
+#include <bits/stdc++.h>
+#define pb push_back
+#define mp make_pair
+#define fr first
+#define sc second
+#define pyes cout << "YES" //"Yes"
+#define pno cout << "NO"   //"No"
+#define fastio ios_base::sync_with_stdio(0)
+#define untie cin.tie(0)
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define repe(i, n) for (int i = 1; i <= (n); i++)
+#define rrep(i, n) for (int i = (n)-1; i >= 0; i--)
+#define rrepe(i, n) for (int i = (n); i > 0; i--)
+#define ms(a, b) memset(a, b, sizeof a)
+#define a2z(x) (x).begin(), (x).end()
+#define z2a(x) (x).rbegin(), (x).rend()
+#define gsz(v) ((int)(v).size())
+#define MOD 1000000007 //998244353
+#define MAX 1000005
+
+#if !defined(__INTERACTIVE__) && !defined(__DEBUG__)
+#define endl "\n"
+#endif
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+using vb = vector<bool>;
+using vi = vector<int>;
+using vl = vector<ll>;
+using vpii = vector<pair<int, int>>;
+
+using vvb = vector<vector<bool>>;
+using vvi = vector<vector<int>>;
+using vvl = vector<vector<ll>>;
+using vvpii = vector<vector<pair<int, int>>>;
+
+using mii = map<int, int>;
+using umii = unordered_map<int, int>;
+using seti = set<int>;
+using useti = unordered_set<int>;
+
+inline void __run_test()
+{
+    ll x, y, a, b, res = 0;
+    cin >> x >> y >> a >> b;
+    if (a == b)
+    {
+        cout << min(x, y) / a << endl;
+        return;
+    }
+    if (a < b)
+        swap(a, b);
+    auto check = [=](ll n)
+    {
+        ll ri = x - n * b;
+        ll le = n * a - y;
+        if (ri < 0)
+            return false;
+        le = max(0LL, le);
+        ll extra = le % (a - b);
+        ll num = le + (a - b - extra) % (a - b);
+        if (num > ri || num > (a - b) * n)
+            return false;
+        return true;
+    };
+    int l = 0;
+    int r = min(x, y);
+    while (l < r)
+    {
+        int mid = (l + r + 1) >> 1;
+        if (check(mid))
+            l = mid;
+        else
+            r = mid - 1;
+    }
+    cout << l << endl;
+}
+
+int main()
+{
+    fastio;
+#if !defined(__INTERACTIVE__) && !defined(__DEBUG__)
+    untie;
+#ifdef LOCAL_OUTPUT
+    freopen(LOCAL_OUTPUT, "w", stdout);
+#endif
+#ifdef LOCAL_INPUT
+    freopen(LOCAL_INPUT, "r", stdin);
+#endif
+#endif
+    int __tests;
+    cin >> __tests;
+    while (__tests--)
+        __run_test();
+}
