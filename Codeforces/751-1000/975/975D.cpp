0bcc3c0b27804442f95d4e263436cd192cab48a9
mwashief <muwashief@gmail.com>
2021-07-15 14:52:00 +0600

    Codeforces 975D

---
+++ Codeforces/751-1000/975/975D.cpp
@@ -0,0 +1,93 @@
+/*
+    Washief Hossain Mugdho
+    15 July 2021
+    Codeforces 751-1000 975 975D
+*/
+
+#ifndef __DEBUG__
+#pragma GCC optimize("Ofast")
+#pragma GCC optimization("unroll-loops, no-stack-protector")
+#pragma GCC target("avx,avx2,fma")
+#endif
+
+#undef __INTERACTIVE__
+
+#include <bits/stdc++.h>
+#define pb push_back
+#define mp make_pair
+#define fr first
+#define sc second
+#define pyes cout << "YES" //"Yes"
+#define pno cout << "NO"   //"No"
+#define fastio ios_base::sync_with_stdio(0)
+#define untie cin.tie(0)
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define repe(i, n) for (int i = 1; i <= (n); i++)
+#define rrep(i, n) for (int i = (n)-1; i >= 0; i--)
+#define rrepe(i, n) for (int i = (n); i > 0; i--)
+#define ms(a, b) memset(a, b, sizeof a)
+#define a2z(x) (x).begin(), (x).end()
+#define z2a(x) (x).rbegin(), (x).rend()
+#define gsz(v) ((int)(v).size())
+#define MOD 1000000007 //998244353
+#define MAX 2000000000000000000LL
+
+#if !defined(__INTERACTIVE__) && !defined(__DEBUG__)
+#define endl "\n"
+#endif
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+using vb = vector<bool>;
+using vi = vector<int>;
+using vl = vector<ll>;
+using vpii = vector<pair<int, int>>;
+
+using vvb = vector<vector<bool>>;
+using vvi = vector<vector<int>>;
+using vvl = vector<vector<ll>>;
+using vvpii = vector<vector<pair<int, int>>>;
+
+using mii = map<int, int>;
+using umii = unordered_map<int, int>;
+using seti = set<int>;
+using useti = unordered_set<int>;
+
+int main()
+{
+    fastio;
+#if !defined(__INTERACTIVE__) && !defined(__DEBUG__)
+    untie;
+#ifdef LOCAL_OUTPUT
+    freopen(LOCAL_OUTPUT, "w", stdout);
+#endif
+#ifdef LOCAL_INPUT
+    freopen(LOCAL_INPUT, "r", stdin);
+#endif
+#endif
+    int n;
+    ll a, vx, vy;
+    cin >> n >> a >> vx;
+    map<ll, int> res;
+    map<pll, int> duplicates;
+
+    ll ans = 0;
+
+    rep(i, n)
+    {
+        cin >> vx >> vx >> vy;
+        vx *= a;
+        ans -= duplicates[{vx, vy}];
+        ans += res[vx - vy];
+        duplicates[{vx, vy}]++;
+        res[vx - vy]++;
+    }
+
+    cout << (ans << 1) << endl;
+}
