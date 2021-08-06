47375cda094a1b0df1e2387dd7797a4be9e9637f
mwashief <muwashief@gmail.com>
2021-08-06 07:21:48 +0600

    Codeforces 1359D

---
+++ Codeforces/1251-1500/1359/1359D.cpp
@@ -0,0 +1,165 @@
+/*
+    Washief Hossain Mugdho
+    06 August 2021
+    Codeforces 1251-1500 1359 1359D
+*/
+
+#ifndef __DEBUG__
+#pragma GCC optimize("Ofast")
+#pragma GCC optimization("unroll-loops, no-stack-protector")
+#pragma GCC target("avx,avx2,fma")
+#endif
+
+#undef __INTERACTIVE__
+
+#include <bits/stdc++.h>
+#define pb push_back
+#define mp make_pair
+#define fr first
+#define sc second
+#define pyes cout << "YES" //"Yes"
+#define pno cout << "NO"   //"No"
+#define fastio ios_base::sync_with_stdio(0)
+#define untie cin.tie(0)
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define repe(i, n) for (int i = 1; i <= (n); i++)
+#define rrep(i, n) for (int i = (n)-1; i >= 0; i--)
+#define rrepe(i, n) for (int i = (n); i > 0; i--)
+#define ms(a, b) memset(a, b, sizeof a)
+#define a2z(x) (x).begin(), (x).end()
+#define z2a(x) (x).rbegin(), (x).rend()
+#define gsz(v) ((int)(v).size())
+#define MOD 1000000007 //998244353
+#define MAX 1000005
+
+#if !defined(__INTERACTIVE__) && !defined(__DEBUG__)
+#define endl "\n"
+#endif
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+using vb = vector<bool>;
+using vi = vector<int>;
+using vl = vector<ll>;
+using vpii = vector<pair<int, int>>;
+
+using vvb = vector<vector<bool>>;
+using vvi = vector<vector<int>>;
+using vvl = vector<vector<ll>>;
+using vvpii = vector<vector<pair<int, int>>>;
+
+using mii = map<int, int>;
+using umii = unordered_map<int, int>;
+using seti = set<int>;
+using useti = unordered_set<int>;
+
+template <class T, class OP>
+class SegmentTree
+{
+    vector<T> t;
+    void build()
+    {
+        for (int i = n - 1; i > 0; --i)
+            t[i] = OP()(t[i << 1], t[i << 1 | 1]);
+    }
+
+public:
+    int n;
+
+    SegmentTree(int nn)
+    {
+        n = nn;
+        t = vector<T>(n << 1);
+    }
+
+    SegmentTree(vector<T> v)
+    {
+        n = v.size();
+        t = vector<T>(n << 1);
+        for (int i = 0; i < n; ++i)
+            t[i + n] = v[i];
+        build();
+    }
+
+    void update(int p, T value)
+    {
+        for (t[p += n] = value; p > 1; p >>= 1)
+            t[p >> 1] = OP()(t[p], t[p ^ 1]);
+    }
+
+    T query(int l, int r)
+    {
+        r++;
+        T res = OP::initVal;
+        for (l += n, r += n; l < r; l >>= 1, r >>= 1)
+        {
+            if (l & 1)
+                res = OP()(res, t[l++]);
+            if (r & 1)
+                res = OP()(res, t[--r]);
+        }
+        return res;
+    }
+};
+
+template <class T>
+class Max
+{
+public:
+    static const T initVal = numeric_limits<T>::min();
+    T operator()(T x, T y)
+    {
+        return max(x, y);
+    }
+};
+
+int main()
+{
+    fastio;
+#if !defined(__INTERACTIVE__) && !defined(__DEBUG__)
+    untie;
+#ifdef LOCAL_OUTPUT
+    freopen(LOCAL_OUTPUT, "w", stdout);
+#endif
+#ifdef LOCAL_INPUT
+    freopen(LOCAL_INPUT, "r", stdin);
+#endif
+#endif
+    int n;
+    cin >> n;
+    vi a(n), nex(n, n), prefixSum(n);
+    rep(i, n) cin >> a[i];
+    stack<int> s;
+    s.push(0);
+    prefixSum[0] = a[0];
+    repe(i, n - 1)
+    {
+        prefixSum[i] = prefixSum[i - 1] + a[i];
+        while (s.size() && a[i] > a[s.top()])
+        {
+            nex[s.top()] = i;
+            s.pop();
+        }
+        s.push(i);
+    }
+    int res = 0;
+    SegmentTree<int, Max<int>> st(prefixSum);
+    int taken = 0;
+    rep(i, n)
+    {
+        int now = i;
+        while (now < n)
+        {
+            res = max(res, st.query(now, nex[now] - 1) - a[now] - taken);
+            now = nex[now];
+        }
+        taken += a[i];
+    }
+    cout << res << endl;
+}
