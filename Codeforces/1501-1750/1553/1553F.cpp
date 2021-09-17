7e5b16bad543f170ebb0a24161ed6e963ba0c9da
mwashief <muwashief@gmail.com>
2021-09-18 04:05:58 +0600

    Codeforces 1553F

---
+++ Codeforces/1501-1750/1553/1553F.cpp
@@ -0,0 +1,276 @@
+/*
+    Washief Hossain Mugdho
+    18 September 2021
+    Codeforces 1501-1750 1553 1553F
+*/
+
+#ifndef __DEBUG__
+#pragma GCC optimize("Ofast")
+#pragma GCC optimization("unroll-loops, no-stack-protector")
+#pragma GCC target("avx,avx2,fma")
+#endif
+
+#undef __INTERACTIVE__
+
+#include <bits/stdc++.h>
+#define pb push_back
+#define mp make_pair
+#define fr first
+#define sc second
+#define pyes cout << "YES" //"Yes"
+#define pno cout << "NO"   //"No"
+#define fastio ios_base::sync_with_stdio(0)
+#define untie cin.tie(0)
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define repe(i, n) for (int i = 1; i <= (n); i++)
+#define rrep(i, n) for (int i = (n)-1; i >= 0; i--)
+#define rrepe(i, n) for (int i = (n); i > 0; i--)
+#define ms(a, b) memset(a, b, sizeof a)
+#define a2z(x) (x).begin(), (x).end()
+#define z2a(x) (x).rbegin(), (x).rend()
+#define gsz(v) ((int)(v).size())
+#define MOD 1000000007 //998244353
+#define MAX 300005LL
+
+#if !defined(__INTERACTIVE__) && !defined(__DEBUG__)
+#define endl "\n"
+#endif
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+using vb = vector<bool>;
+using vi = vector<int>;
+using vl = vector<ll>;
+using vpii = vector<pair<int, int>>;
+
+using vvb = vector<vector<bool>>;
+using vvi = vector<vector<int>>;
+using vvl = vector<vector<ll>>;
+using vvpii = vector<vector<pair<int, int>>>;
+
+using mii = map<int, int>;
+using umii = unordered_map<int, int>;
+using seti = set<int>;
+using useti = unordered_set<int>;
+
+template <class T, class OP>
+class SegmentTree
+{
+    vector<T> t;
+    void build()
+    {
+        for (int i = n - 1; i > 0; --i)
+            t[i] = OP()(t[i << 1], t[i << 1 | 1]);
+    }
+
+public:
+    int n;
+
+    SegmentTree(int nn)
+    {
+        n = nn;
+        t = vector<T>(n << 1);
+    }
+
+    SegmentTree(vector<T> v)
+    {
+        n = v.size();
+        t = vector<T>(n << 1);
+        for (int i = 0; i < n; ++i)
+            t[i + n] = v[i];
+        build();
+    }
+
+    void update(int p, T value)
+    {
+        for (t[p += n] = value; p > 1; p >>= 1)
+            t[p >> 1] = OP()(t[p], t[p ^ 1]);
+    }
+
+    T query(int l, int r)
+    {
+        r++;
+        T res = OP::initVal;
+        for (l += n, r += n; l < r; l >>= 1, r >>= 1)
+        {
+            if (l & 1)
+                res = OP()(res, t[l++]);
+            if (r & 1)
+                res = OP()(res, t[--r]);
+        }
+        return res;
+    }
+};
+
+template <class T>
+class Add
+{
+public:
+    static const T initVal = T(0);
+    T operator()(T x, T y)
+    {
+        return x + y;
+    }
+};
+
+template <class T>
+T OP(T a, T b)
+{
+    return a + b;
+}
+
+template <class T>
+class LazySegmentTree
+{
+private:
+    //change here
+    void propagate(int node, T val, int nodes)
+    {
+        tree[node] += val * (T)nodes;
+        lazy[node] += val;
+    }
+
+    T buildUtil(vector<T> &a, int node, int l, int r)
+    {
+        if (l == r)
+        {
+            tree[node] = a[l];
+            return tree[node];
+        }
+        int mid = (l + r) >> 1;
+        int leftNode = (node << 1) + 1;
+        int rightNode = leftNode + 1;
+        return tree[node] = OP(buildUtil(a, leftNode, l, mid), buildUtil(a, rightNode, mid + 1, r));
+    }
+
+    T queryUtil(int node, int l, int r, int ql, int qr)
+    {
+        //change here
+        if (ql > r || qr < l)
+            return (T)0;
+        if (l >= ql && r <= qr)
+            return tree[node];
+
+        int mid = (l + r) >> 1;
+        int leftNode = (node << 1) + 1;
+        int rightNode = leftNode + 1;
+        if (lazy[node]) //change here
+        {
+            propagate(leftNode, lazy[node], mid - l + 1);
+            propagate(rightNode, lazy[node], r - mid);
+            lazy[node] = (T)0; // change here
+        }
+        return OP(queryUtil(leftNode, l, mid, ql, qr), queryUtil(rightNode, mid + 1, r, ql, qr));
+    }
+
+    T updateUtil(int node, int l, int r, int ql, int qr, T val)
+    {
+        if (ql > r || qr < l)
+            return tree[node];
+        if (l >= ql && r <= qr)
+        {
+            //change here
+            tree[node] = OP(tree[node], val * (r - l + 1));
+            lazy[node] = OP(lazy[node], val);
+            return tree[node];
+        }
+
+        int mid = (l + r) >> 1;
+        int leftNode = (node << 1) + 1;
+        int rightNode = leftNode + 1;
+        if (lazy[node]) //change here
+        {
+            propagate(leftNode, lazy[node], mid - l + 1);
+            propagate(rightNode, lazy[node], r - mid);
+            lazy[node] = 0; //change here
+        }
+        return tree[node] = OP(updateUtil(leftNode, l, mid, ql, qr, val), updateUtil(rightNode, mid + 1, r, ql, qr, val));
+    }
+
+public:
+    int n;
+    vector<T> tree;
+    vector<T> lazy;
+    LazySegmentTree(int n)
+    {
+        this->n = n;
+        tree = vector<T>(4 * n);
+        lazy = vector<T>(4 * n);
+        //change here
+        vector<T> a(n);
+        build(a);
+    }
+
+    void build(vector<T> &a)
+    {
+        buildUtil(a, 0, 0, n - 1);
+    }
+
+    LazySegmentTree(vector<T> &a)
+    {
+        this->n = a.size();
+        tree = vector<T>(4 * n);
+        lazy = vector<T>(4 * n);
+        build(a);
+    }
+
+    T query(int l, int r)
+    {
+        return queryUtil(0, 0, n - 1, l, r);
+    }
+
+    void update(int l, int r, T val)
+    {
+        updateUtil(0, 0, n - 1, l, r, val);
+    }
+};
+
+int main()
+{
+    fastio;
+#if !defined(__INTERACTIVE__) && !defined(__DEBUG__)
+    untie;
+#ifdef LOCAL_OUTPUT
+    freopen(LOCAL_OUTPUT, "w", stdout);
+#endif
+#ifdef LOCAL_INPUT
+    freopen(LOCAL_INPUT, "r", stdin);
+#endif
+#endif
+    int n;
+    cin >> n;
+
+    SegmentTree<ll, Add<ll>> sum(MAX);
+    SegmentTree<ll, Add<ll>> c(MAX);
+    LazySegmentTree<ll> futureMinus(MAX);
+    ll cumsum = 0;
+    rep(i, n)
+    {
+        ll x;
+        cin >> x;
+        ll mul = 1;
+        ll res = sum.query(0, x - 1) + x * i - futureMinus.query(x, x);
+        while (x * mul < MAX)
+        {
+            int l = x * mul;
+            int r = min(MAX - 1, x * (mul + 1) - 1);
+            ll howMany = c.query(l, r);
+            res += sum.query(l, r);
+            res -= howMany * x * mul;
+            futureMinus.update(l, r, mul * x);
+            mul++;
+        }
+
+        cumsum += res;
+        c.update(x, 1);
+        sum.update(x, x);
+        cout << cumsum << " ";
+    }
+    cout << endl;
+}
