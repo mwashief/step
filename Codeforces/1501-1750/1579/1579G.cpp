697c722cbff7f2071f652f5fcf8d6201a7d1138e
mwashief <muwashief@gmail.com>
2021-10-06 21:28:24 +0600

    Codeforces 1579G

---
+++ Codeforces/1501-1750/1579/1579G.cpp
@@ -0,0 +1,104 @@
+/*
+    Washief Hossain Mugdho
+    06 October 2021
+    Codeforces 1501-1750 1579 1579G
+*/
+
+#ifndef __DEBUG__
+#pragma GCC optimize("Ofast")
+#pragma GCC optimization("unroll-loops, no-stack-protector")
+#pragma GCC target("avx,avx2,fma")
+#endif
+
+#undef __INTERACTIVE__
+
+#include <bits/stdc++.h>
+#define pb push_back
+#define mp make_pair
+#define fr first
+#define sc second
+#define pyes cout << "YES" //"Yes"
+#define pno cout << "NO"   //"No"
+#define fastio ios_base::sync_with_stdio(0)
+#define untie cin.tie(0)
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define repe(i, n) for (int i = 1; i <= (n); i++)
+#define rrep(i, n) for (int i = (n)-1; i >= 0; i--)
+#define rrepe(i, n) for (int i = (n); i > 0; i--)
+#define ms(a, b) memset(a, b, sizeof a)
+#define a2z(x) (x).begin(), (x).end()
+#define z2a(x) (x).rbegin(), (x).rend()
+#define gsz(v) ((int)(v).size())
+#define MOD 1000000007 //998244353
+#define MAX 1000005
+
+#if !defined(__INTERACTIVE__) && !defined(__DEBUG__)
+#define endl "\n"
+#endif
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+using vb = vector<bool>;
+using vi = vector<int>;
+using vl = vector<ll>;
+using vpii = vector<pair<int, int>>;
+
+using vvb = vector<vector<bool>>;
+using vvi = vector<vector<int>>;
+using vvl = vector<vector<ll>>;
+using vvpii = vector<vector<pair<int, int>>>;
+
+using mii = map<int, int>;
+using umii = unordered_map<int, int>;
+using seti = set<int>;
+using useti = unordered_set<int>;
+int dp_old[2005], dp_new[2005];
+
+inline void __run_test()
+{
+    int n;
+    cin >> n;
+    fill(dp_old, dp_old + 2005, MAX);
+
+    dp_old[0] = 0;
+    rep(i, n)
+    {
+        int x;
+        cin >> x;
+        fill(dp_new, dp_new + 2005, MAX);
+        rep(j, x + 1) dp_new[0] = min(dp_new[0], dp_old[j] + x - j);
+        rep(j, 2005)
+        {
+            if (j + x < 2005)
+                dp_new[j] = min(dp_new[j], dp_old[j + x]);
+            if (j - x >= 0)
+                dp_new[j] = min(dp_new[j], max(dp_old[j - x], j));
+        }
+        rep(j, 2005) dp_old[j] = dp_new[j];
+    }
+    cout << *min_element(dp_old, dp_old + 2005) << endl;
+}
+
+int main()
+{
+    fastio;
+#if !defined(__INTERACTIVE__) && !defined(__DEBUG__)
+    untie;
+#ifdef LOCAL_OUTPUT
+    freopen(LOCAL_OUTPUT, "w", stdout);
+#endif
+#ifdef LOCAL_INPUT
+    freopen(LOCAL_INPUT, "r", stdin);
+#endif
+#endif
+    int __tests;
+    cin >> __tests;
+    while (__tests--)
+        __run_test();
+}
