07785e624e6196a7c12eb6d30631b6c766969c40
mwashief <muwashief@gmail.com>
2021-10-09 08:55:57 +0600

    Codeforces 1594C

---
+++ Codeforces/1501-1750/1594/1594C.cpp
@@ -0,0 +1,125 @@
+/*
+    Washief Hossain Mugdho
+    08 October 2021
+    Codeforces 1501-1750 1594 1594C
+*/
+
+#ifndef __DEBUG__
+#pragma GCC optimize("Ofast")
+#pragma GCC optimization("unroll-loops, no-stack-protector")
+#pragma GCC target("avx,avx2,fma")
+#endif
+
+#undef __INTERACTIVE__
+
+#include <bits/stdc++.h>
+#define pb push_back
+#define mp make_pair
+#define fr first
+#define sc second
+#define pyes cout << "YES" //"Yes"
+#define pno cout << "NO"   //"No"
+#define fastio ios_base::sync_with_stdio(0)
+#define untie cin.tie(0)
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define repe(i, n) for (int i = 1; i <= (n); i++)
+#define rrep(i, n) for (int i = (n)-1; i >= 0; i--)
+#define rrepe(i, n) for (int i = (n); i > 0; i--)
+#define ms(a, b) memset(a, b, sizeof a)
+#define a2z(x) (x).begin(), (x).end()
+#define z2a(x) (x).rbegin(), (x).rend()
+#define gsz(v) ((int)(v).size())
+#define MOD 1000000007 //998244353
+#define MAX 300008
+
+#if !defined(__INTERACTIVE__) && !defined(__DEBUG__)
+#define endl "\n"
+#endif
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+using vb = vector<bool>;
+using vi = vector<int>;
+using vl = vector<ll>;
+using vpii = vector<pair<int, int>>;
+
+using vvb = vector<vector<bool>>;
+using vvi = vector<vector<int>>;
+using vvl = vector<vector<ll>>;
+using vvpii = vector<vector<pair<int, int>>>;
+
+using mii = map<int, int>;
+using umii = unordered_map<int, int>;
+using seti = set<int>;
+using useti = unordered_set<int>;
+
+inline void __run_test()
+{
+    int n;
+    cin >> n;
+
+    char c;
+    cin >> c;
+    int notc = 0;
+    string s;
+    cin >> s;
+    rep(i, n) if (s[i] != c) notc++;
+
+    if (notc == 0)
+    {
+        cout << 0 << endl;
+        return;
+    }
+
+    for (int i = 2; i <= n; i++)
+    {
+        int d = 0;
+        for (int j = i; j <= n; j += i)
+            d += (s[j - 1] != c);
+        if (!d)
+        {
+            cout << 1 << endl;
+            cout << i << endl;
+            return;
+        }
+    }
+
+    auto getOne = [&]() -> int
+    {
+        repe(i, n) if (n % i) return i;
+        return 0;
+    };
+
+    cout << 2 << endl;
+    cout << n << " " << getOne() << endl;
+}
+
+int main()
+{
+    fastio;
+#if !defined(__INTERACTIVE__) && !defined(__DEBUG__)
+    untie;
+#ifdef LOCAL_OUTPUT
+    freopen(LOCAL_OUTPUT, "w", stdout);
+#endif
+#ifdef LOCAL_INPUT
+    freopen(LOCAL_INPUT, "r", stdin);
+#endif
+#endif
+    int __tests;
+    cin >> __tests;
+    while (__tests--)
+        __run_test();
+}
+
+/*
+-> Check for data types
+-> Fix macro
+-> Run test case
+*/
