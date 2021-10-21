26607decb22d7a6e5cbd03e2cf9e0de111e7730d
mwashief <muwashief@gmail.com>
2021-10-21 21:24:40 +0600

    HackerEarth E

---
+++ HackerEarth/oct-circuit/E.cpp
@@ -0,0 +1,149 @@
+/*
+    Washief Hossain Mugdho
+    21 October 2021
+    HackerEarth oct-circuit E
+*/
+
+#ifndef __DEBUG__
+#pragma GCC optimize("Ofast")
+#pragma GCC optimization("unroll-loops, no-stack-protector")
+#pragma GCC target("avx,avx2,fma")
+#endif
+
+#undef __INTERACTIVE__
+
+#include <bits/stdc++.h>
+#define pb push_back
+#define mp make_pair
+#define fr first
+#define sc second
+#define pyes cout << "YES" //"Yes"
+#define pno cout << "NO"   //"No"
+#define fastio ios_base::sync_with_stdio(0)
+#define untie cin.tie(0)
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define repe(i, n) for (int i = 1; i <= (n); i++)
+#define rrep(i, n) for (int i = (n)-1; i >= 0; i--)
+#define rrepe(i, n) for (int i = (n); i > 0; i--)
+#define ms(a, b) memset(a, b, sizeof a)
+#define a2z(x) (x).begin(), (x).end()
+#define z2a(x) (x).rbegin(), (x).rend()
+#define gsz(v) ((int)(v).size())
+#define MOD 1000000007 //998244353
+#define MAX 1000005
+
+#if !defined(__INTERACTIVE__) && !defined(__DEBUG__)
+#define endl "\n"
+#endif
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+using vb = vector<bool>;
+using vi = vector<int>;
+using vl = vector<ll>;
+using vpii = vector<pair<int, int>>;
+
+using vvb = vector<vector<bool>>;
+using vvi = vector<vector<int>>;
+using vvl = vector<vector<ll>>;
+using vvpii = vector<vector<pair<int, int>>>;
+
+using mii = map<int, int>;
+using umii = unordered_map<int, int>;
+using seti = set<int>;
+using useti = unordered_set<int>;
+
+namespace std
+{
+
+    template <class Fun>
+    class y_combinator_result
+    {
+        Fun fun_;
+
+    public:
+        template <class T>
+        explicit y_combinator_result(T &&fun) : fun_(std::forward<T>(fun)) {}
+
+        template <class... Args>
+        decltype(auto) operator()(Args &&...args)
+        {
+            return fun_(std::ref(*this), std::forward<Args>(args)...);
+        }
+    };
+
+    template <class Fun>
+    decltype(auto) y_combinator(Fun &&fun)
+    {
+        return y_combinator_result<std::decay_t<Fun>>(std::forward<Fun>(fun));
+    }
+
+    template <class T>
+    T &unmove(T &&t) { return t; }
+
+}
+
+inline void __run_test()
+{
+    ll n;
+    cin >> n;
+    vl t(3), c(3);
+    vvl a(3, vl(3));
+    rep(i, 3) cin >> t[i];
+    rep(i, 3) cin >> c[i];
+    rep(i, 3) rep(j, 3) cin >> a[i][j];
+    ll res = INT64_MAX;
+    y_combinator([&](auto self, vl x, vl y, ll val) -> void
+                 {
+                     ll rem = 0;
+                     rep(i, 3) rem += x[i];
+                     if (!rem)
+                     {
+                         res = min(res, val);
+                         return;
+                     }
+                     rep(i, 3) rep(j, 3) if (x[i] != 0 && y[j] != 0)
+                     {
+                         ll k = min(x[i], y[j]);
+                         x[i] -= k;
+                         y[j] -= k;
+                         self(x, y, val + k * a[i][j]);
+                         x[i] += k;
+                         y[j] += k;
+                     }
+                 })(t, c, 0LL);
+    cout << res << endl;
+}
+
+int main()
+{
+    fastio;
+#if !defined(__INTERACTIVE__) && !defined(__DEBUG__)
+    untie;
+#ifdef LOCAL_OUTPUT
+    freopen(LOCAL_OUTPUT, "w", stdout);
+#endif
+#ifdef LOCAL_INPUT
+    freopen(LOCAL_INPUT, "r", stdin);
+#endif
+#endif
+    int __tests;
+    cin >> __tests;
+    while (__tests--)
+        __run_test();
+}
+
+/*
+-> Check for data types
+-> Fix macro
+-> Run test case
+-> Check the copied part
+-> Set or multiset?
+-> Check statement order
+*/
