c061c066612b6582a51a742c82ced053a8a880db
mwashief <muwashief@gmail.com>
2021-10-31 17:12:13 +0600

    Codeforces 1604E

---
+++ Codeforces/1501-1750/1604/1604E.cpp
@@ -0,0 +1,283 @@
+/*
+    Washief Hossain Mugdho
+    30 October 2021
+    Codeforces 1501-1750 1604 1604E
+*/
+
+#ifndef __DEBUG__
+#pragma GCC optimize("Ofast")
+#pragma GCC optimization("unroll-loops, no-stack-protector")
+#pragma GCC target("avx,avx2,fma")
+#endif
+
+#undef __INTERACTIVE__
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define pb push_back
+#define mp make_pair
+#define fr first
+#define sc second
+#define pyes cout << "YES" //"Yes"
+#define pno cout << "NO"   //"No"
+#define fastio ios_base::sync_with_stdio(0)
+#define untie cin.tie(0)
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define repe(i, n) for (int i = 1; i <= (n); i++)
+#define rrep(i, n) for (int i = (n)-1; i >= 0; i--)
+#define rrepe(i, n) for (int i = (n); i > 0; i--)
+#define ms(a, b) memset(a, b, sizeof a)
+#define a2z(x) (x).begin(), (x).end()
+#define z2a(x) (x).rbegin(), (x).rend()
+#define gsz(v) ((int)(v).size())
+#define MOD 998244353
+#define MAX 1000005
+
+#if !defined(__INTERACTIVE__) && !defined(__DEBUG__)
+#define endl "\n"
+#endif
+
+using namespace std;
+using namespace __gnu_pbds;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+using vb = vector<bool>;
+using vi = vector<int>;
+using vl = vector<ll>;
+using vpii = vector<pair<int, int>>;
+
+using vvb = vector<vector<bool>>;
+using vvi = vector<vector<int>>;
+using vvl = vector<vector<ll>>;
+using vvpii = vector<vector<pair<int, int>>>;
+
+using mii = map<int, int>;
+
+using seti = set<int>;
+using useti = unordered_set<int>;
+template <int mod>
+struct Mod
+{
+    long long x;
+    Mod() : x(0) {}
+    Mod(long long xx) : x(xx)
+    {
+        if (abs(x) >= mod)
+            x %= mod;
+        if (x < 0)
+            x += mod;
+    }
+    operator int() const { return x; }
+    operator long long() const { return x; }
+    Mod operator+(const Mod &a) const
+    {
+        Mod n;
+        n.x = x + a.x;
+        if (n.x >= mod)
+            n.x -= mod;
+        return n;
+    }
+    Mod operator-(const Mod &a) const
+    {
+        Mod n;
+        n.x = x - a.x;
+        if (n.x < 0)
+            n.x += mod;
+        return n;
+    }
+    Mod operator*(const Mod &a) const
+    {
+        return x * a.x;
+    }
+    Mod operator+=(const Mod &a)
+    {
+        x += a.x;
+        if (x >= mod)
+            x -= mod;
+        return *this;
+    }
+    Mod operator-=(const Mod &a)
+    {
+        x -= a.x;
+        if (x < 0)
+            x += mod;
+        return *this;
+    }
+    Mod operator++()
+    {
+        *this += 1;
+        return *this;
+    }
+    Mod operator++(int) //postfix
+    {
+        Mod temp = *this;
+        *this += 1;
+        return temp;
+    }
+    Mod operator--()
+    {
+        *this -= 1;
+        return *this;
+    }
+    Mod operator--(int) //postfix
+    {
+        Mod temp = *this;
+        *this -= 1;
+        return temp;
+    }
+    Mod operator*=(const Mod &a)
+    {
+        x = (x * a.x) % mod;
+        return *this;
+    }
+    Mod pow(long long b) const
+    {
+        Mod ans = 1;
+        Mod a = *this;
+        while (b > 0)
+        {
+            if (b & 1)
+                ans *= a;
+            a *= a;
+            b /= 2;
+        }
+        return ans;
+    }
+    Mod inv() const
+    {
+        return pow(mod - 2);
+    }
+    Mod operator/(const Mod &a) const
+    {
+        return (*this) * a.inv();
+    }
+    Mod operator/=(const Mod &a)
+    {
+        return (*this) *= a.inv();
+    }
+    bool operator==(const Mod &o) const
+    {
+        return x == o.x;
+    }
+    bool operator!=(const Mod &o) const
+    {
+        return x != o.x;
+    }
+    long long operator()() const
+    {
+        return x;
+    }
+    template <int _mod>
+    friend ostream &operator<<(ostream &os, const Mod<_mod> &num)
+    {
+        os << num.x;
+        return os;
+    }
+    template <int _mod>
+    friend istream &operator>>(istream &is, Mod<_mod> &num)
+    {
+        is >> num.x;
+        if (abs(num.x) >= mod)
+            num.x %= mod;
+        if (num.x < 0)
+            num.x += mod;
+        return is;
+    }
+};
+using base = Mod<MOD>;
+using vbase = vector<base>;
+using vvbase = vector<vector<base>>;
+base two = 2;
+
+using umii = vector<pair<int, pair<int, base>>>;
+
+pair<int, base> operator+(pair<int, base> a, pair<int, base> b)
+{
+    pair<int, base> ans = {0, 0};
+    ans.fr = a.fr + b.fr;
+    ans.sc = a.sc + b.sc;
+    return ans;
+}
+
+void insert(umii *a, int k, pair<int, base> b)
+{
+    if (a->size() == 0)
+    {
+        a->pb(mp(k, b));
+        return;
+    }
+    if (a->back().fr == k)
+        a->back().sc = a->back().sc + b;
+    else
+        a->pb(mp(k, b));
+}
+
+umii *possible = new umii();
+umii *temp = new umii();
+inline void __run_test()
+{
+    int n;
+    cin >> n;
+    vi a(n);
+    rep(i, n) cin >> a[i];
+    possible->clear();
+    base res = 0;
+    rrep(i, n)
+    {
+        temp->clear();
+
+        for (auto d : *possible)
+        {
+            if (d.fr >= a[i])
+            {
+                if (i)
+                    insert(temp, min(a[i], a[i - 1]), d.sc);
+                res += base(d.sc.sc);
+            }
+            else
+            {
+                int divs = (a[i] + d.fr - 1) / d.fr;
+                res += base(d.sc.fr) * base(divs - 1) + base(d.sc.sc);
+                if (i)
+                    insert(temp, min(a[i - 1], a[i] / divs), make_pair(d.sc.fr, d.sc.sc + base(d.sc.fr) * base(divs - 1)));
+            }
+        }
+        insert(temp, min(a[i], a[i - 1]), mp(1, base(0)));
+        swap(possible, temp);
+    }
+    cout << res << endl;
+}
+
+int main()
+{
+    fastio;
+#if !defined(__INTERACTIVE__) && !defined(__DEBUG__)
+    untie;
+#ifdef LOCAL_OUTPUT
+    freopen(LOCAL_OUTPUT, "w", stdout);
+#endif
+#ifdef LOCAL_INPUT
+    freopen(LOCAL_INPUT, "r", stdin);
+#endif
+#endif
+
+    int __tests;
+    cin >> __tests;
+    while (__tests--)
+        __run_test();
+}
+
+/*
+-> Check for data types
+-> Fix macro
+-> Run test case
+-> Check the copied part
+-> Set or multiset?
+-> Check statement order
+*/
