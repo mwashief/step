7298a9419507fdc09a8126997048883acda52218
mwashief <muwashief@gmail.com>
2021-11-01 14:38:26 +0600

    Codeforces 1555A

---
+++ Codeforces/1501-1750/1555/1555A.cpp
@@ -0,0 +1,117 @@
+/*
+    Washief Hossain Mugdho
+    01 November 2021
+    Codeforces 1501-1750 1555 1555A
+*/
+
+#ifndef __DEBUG__
+#pragma GCC optimize("Ofast")
+#pragma GCC optimization("unroll-loops, no-stack-protector")
+#pragma GCC target("avx,avx2,fma")
+#endif
+
+#undef __INTERACTIVE__
+
+#include <bits/stdc++.h>
+#define pb push_back
+#define mp make_pair
+#define fr first
+#define sc second
+#define pyes cout << "YES" //"Yes"
+#define pno cout << "NO"   //"No"
+#define fastio ios_base::sync_with_stdio(0)
+#define untie cin.tie(0)
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define repe(i, n) for (int i = 1; i <= (n); i++)
+#define rrep(i, n) for (int i = (n)-1; i >= 0; i--)
+#define rrepe(i, n) for (int i = (n); i > 0; i--)
+#define ms(a, b) memset(a, b, sizeof a)
+#define a2z(x) (x).begin(), (x).end()
+#define z2a(x) (x).rbegin(), (x).rend()
+#define gsz(v) ((int)(v).size())
+#define MOD 1000000007 //998244353
+#define MAX 1000005
+
+#if !defined(__INTERACTIVE__) && !defined(__DEBUG__)
+#define endl "\n"
+#endif
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+using vb = vector<bool>;
+using vi = vector<int>;
+using vl = vector<ll>;
+using vpii = vector<pair<int, int>>;
+
+using vvb = vector<vector<bool>>;
+using vvi = vector<vector<int>>;
+using vvl = vector<vector<ll>>;
+using vvpii = vector<vector<pair<int, int>>>;
+
+using mii = map<int, int>;
+using umii = unordered_map<int, int>;
+using seti = set<int>;
+using useti = unordered_set<int>;
+
+inline void __run_test()
+{
+    ll n;
+    cin >> n;
+    if (n & 1LL)
+        n++;
+    ll tens = n / 10;
+    ll last = n % 10;
+    if (n <= 10)
+    {
+        if (n <= 6)
+            cout << 15 << endl;
+        else if (n <= 8)
+            cout << 20 << endl;
+        else
+            cout << 25 << endl;
+        return;
+    }
+    if (last == 0)
+        cout << tens * 25LL << endl;
+    else if (last == 2)
+        cout << (tens - 1) * 25LL + 30LL << endl;
+    else if (last == 4)
+        cout << (tens - 1) * 25LL + 35LL << endl;
+    else if (last == 6)
+        cout << tens * 25LL + 15LL << endl;
+    else if (last == 8)
+        cout << tens * 25LL + 20LL << endl;
+}
+
+int main()
+{
+    fastio;
+#if !defined(__INTERACTIVE__) && !defined(__DEBUG__)
+    untie;
+#ifdef LOCAL_OUTPUT
+    freopen(LOCAL_OUTPUT, "w", stdout);
+#endif
+#ifdef LOCAL_INPUT
+    freopen(LOCAL_INPUT, "r", stdin);
+#endif
+#endif
+    int __tests;
+    cin >> __tests;
+    while (__tests--)
+        __run_test();
+}
+
+/*
+-> Check for data types
+-> Fix macro
+-> Run test case
+-> Check the copied part
+-> Set or multiset?
+-> Check statement order
+*/
