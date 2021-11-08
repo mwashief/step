e9e600c8f602ffc8874d6898b62b38734c658a26
mwashief <muwashief@gmail.com>
2021-11-08 16:33:04 +0600

    Codeforces 1333F

---
+++ Codeforces/1251-1500/1333/1333F.cpp
@@ -0,0 +1,110 @@
+/*
+    Washief Hossain Mugdho
+    08 November 2021
+    Codeforces 1251-1500 1333 1333F
+*/
+
+#ifndef __DEBUG__
+#pragma GCC optimize("Ofast")
+#pragma GCC optimization("unroll-loops, no-stack-protector")
+#pragma GCC target("avx,avx2,fma")
+#endif
+
+#undef __INTERACTIVE__
+
+#include <bits/stdc++.h>
+#define pb push_back
+#define mp make_pair
+#define fr first
+#define sc second
+#define pyes cout << "YES" //"Yes"
+#define pno cout << "NO"   //"No"
+#define fastio ios_base::sync_with_stdio(0)
+#define untie cin.tie(0)
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define repe(i, n) for (int i = 1; i <= (n); i++)
+#define rrep(i, n) for (int i = (n)-1; i >= 0; i--)
+#define rrepe(i, n) for (int i = (n); i > 0; i--)
+#define ms(a, b) memset(a, b, sizeof a)
+#define a2z(x) (x).begin(), (x).end()
+#define z2a(x) (x).rbegin(), (x).rend()
+#define gsz(v) ((int)(v).size())
+#define MOD 1000000007 //998244353
+#define MAX 1000005
+
+#if !defined(__INTERACTIVE__) && !defined(__DEBUG__)
+#define endl "\n"
+#endif
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+using vb = vector<bool>;
+using vi = vector<int>;
+using vl = vector<ll>;
+using vpii = vector<pair<int, int>>;
+
+using vvb = vector<vector<bool>>;
+using vvi = vector<vector<int>>;
+using vvl = vector<vector<ll>>;
+using vvpii = vector<vector<pair<int, int>>>;
+
+using mii = map<int, int>;
+using umii = unordered_map<int, int>;
+using seti = set<int>;
+using useti = unordered_set<int>;
+
+int main()
+{
+    fastio;
+#if !defined(__INTERACTIVE__) && !defined(__DEBUG__)
+    untie;
+#ifdef LOCAL_OUTPUT
+    freopen(LOCAL_OUTPUT, "w", stdout);
+#endif
+#ifdef LOCAL_INPUT
+    freopen(LOCAL_INPUT, "r", stdin);
+#endif
+#endif
+    int n;
+    cin >> n;
+    vi spf(n + 1);
+    iota(a2z(spf), 0);
+    priority_queue<int> pq;
+    pq.push(1);
+    for (int i = 2; i <= n; i++)
+    {
+        pq.push(i / spf[i]);
+        if (spf[i] == i)
+        {
+            for (ll j = 1LL * i * i; j <= n; j += i)
+                spf[j] = min(spf[j], i);
+        }
+    }
+
+    vi ans;
+    while (gsz(pq) >= 2)
+    {
+        auto num = pq.top();
+        pq.pop();
+        ans.pb(num);
+    }
+    reverse(a2z(ans));
+    for (auto x : ans)
+        cout << x << " ";
+    cout << endl;
+}
+
+/*
+-> Check for data types
+-> Fix macro
+-> Run test case
+-> Check the copied part
+-> Set or multiset?
+-> Check statement order
+*/
