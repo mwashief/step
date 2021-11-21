8763550f622393d407e8c5491fcc31f9d94a51fb
mwashief <muwashief@gmail.com>
2021-11-21 21:28:56 +0600

    HackerEarth A

---
+++ HackerEarth/nov-circuit/A.cpp
@@ -0,0 +1,176 @@
+/*
+    Washief Hossain Mugdho
+    20 November 2021
+    HackerEarth nov-circuit A
+*/
+
+#ifndef __DEBUG__
+#pragma GCC optimize("Ofast")
+#pragma GCC optimization("unroll-loops, no-stack-protector")
+#pragma GCC target("avx,avx2,fma")
+#endif
+
+#undef __INTERACTIVE__
+
+#include <bits/stdc++.h>
+#define pb push_back
+#define mp make_pair
+#define fr first
+#define sc second
+#define pyes cout << "YES" //"Yes"
+#define pno cout << "NO"   //"No"
+#define fastio ios_base::sync_with_stdio(0)
+#define untie cin.tie(0)
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define repe(i, n) for (int i = 1; i <= (n); i++)
+#define rrep(i, n) for (int i = (n)-1; i >= 0; i--)
+#define rrepe(i, n) for (int i = (n); i > 0; i--)
+#define ms(a, b) memset(a, b, sizeof a)
+#define a2z(x) (x).begin(), (x).end()
+#define z2a(x) (x).rbegin(), (x).rend()
+#define gsz(v) ((int)(v).size())
+#define MOD 1000000007 //998244353
+#define MAX 1000005
+
+#if !defined(__INTERACTIVE__) && !defined(__DEBUG__)
+#define endl "\n"
+#endif
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+using vb = vector<bool>;
+using vi = vector<int>;
+using vl = vector<ll>;
+using vpii = vector<pair<int, int>>;
+
+using vvb = vector<vector<bool>>;
+using vvi = vector<vector<int>>;
+using vvl = vector<vector<ll>>;
+using vvpii = vector<vector<pair<int, int>>>;
+
+using mii = map<int, int>;
+using umii = unordered_map<int, int>;
+using seti = set<int>;
+using useti = unordered_set<int>;
+
+namespace std
+{
+
+    template <class Fun>
+    class y_combinator_result
+    {
+        Fun fun_;
+
+    public:
+        template <class T>
+        explicit y_combinator_result(T &&fun) : fun_(std::forward<T>(fun)) {}
+
+        template <class... Args>
+        decltype(auto) operator()(Args &&...args)
+        {
+            return fun_(std::ref(*this), std::forward<Args>(args)...);
+        }
+    };
+
+    template <class Fun>
+    decltype(auto) y_combinator(Fun &&fun)
+    {
+        return y_combinator_result<std::decay_t<Fun>>(std::forward<Fun>(fun));
+    }
+
+    template <class T>
+    T &unmove(T &&t) { return t; }
+
+}
+
+inline void __run_test()
+{
+    int n, q;
+    cin >> n;
+    vvi adj(n);
+    vi a(n), tin(n), tout(n);
+    vl cost(n);
+    rep(i, n) cin >> a[i];
+
+    rep(i, n - 1)
+    {
+        int u, v;
+        cin >> u >> v;
+        u--;
+        v--;
+        adj[u].pb(v);
+        adj[v].pb(u);
+    }
+    int t = 0;
+    y_combinator([&](auto self, int node, int par) -> ll
+                 {
+                     tin[node] = t++;
+                     cost[node] = a[node];
+                     for (auto child : adj[node])
+                         if (child != par)
+                             cost[node] += self(child, node);
+                     tout[node] = t++;
+                     return cost[node];
+                 })(0, -1);
+
+    cin >> q;
+    while (q--)
+    {
+        int u, v, x;
+        cin >> u >> v >> x;
+        u--;
+        v--;
+        x--;
+        if ((tin[u] < tin[v] && tout[u] > tout[v]) ||
+            (tin[v] < tin[u] && tout[v] > tout[u]) ||
+            (tin[v] < tin[x] && tout[v] > tout[x]) ||
+            (tin[u] < tin[x] && tout[u] > tout[x]))
+            cout << cost[x] << " ";
+
+        else
+        {
+            bool c = tin[x] < tin[v] && tout[x] > tout[v];
+            bool d = tin[x] < tin[u] && tout[x] > tout[u];
+            if (!(c ^ d))
+                cout << cost[x] << " ";
+            else if (c)
+                cout << cost[x] - cost[v] + cost[u] << " ";
+            else
+                cout << cost[x] - cost[u] + cost[v] << " ";
+        }
+    }
+    cout << endl;
+}
+
+int main()
+{
+    fastio;
+#if !defined(__INTERACTIVE__) && !defined(__DEBUG__)
+    untie;
+#ifdef LOCAL_OUTPUT
+    freopen(LOCAL_OUTPUT, "w", stdout);
+#endif
+#ifdef LOCAL_INPUT
+    freopen(LOCAL_INPUT, "r", stdin);
+#endif
+#endif
+    int __tests;
+    cin >> __tests;
+    while (__tests--)
+        __run_test();
+}
+
+/*
+-> Check for data types
+-> Fix macro
+-> Run test case
+-> Check the copied part
+-> Set or multiset?
+-> Check statement order
+*/
