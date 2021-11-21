2a2fb13187837a401a21a96924ab0d71a113837d
mwashief <muwashief@gmail.com>
2021-11-21 21:29:05 +0600

    HackerEarth D

---
+++ HackerEarth/nov-circuit/D.cpp
@@ -0,0 +1,166 @@
+/*
+    Washief Hossain Mugdho
+    21 November 2021
+    HackerEarth nov-circuit D
+*/
+
+#ifndef __DEBUG__
+#pragma GCC optimize("Ofast")
+#pragma GCC optimization("unroll-loops, no-stack-protector")
+#pragma GCC target("avx,avx2,fma")
+#endif
+
+#undef __INTERACTIVE__
+
+#include <bits/stdc++.h>
+#define pb push_back
+#define mp make_pair
+#define fr first
+#define sc second
+#define pyes cout << "YES" //"Yes"
+#define pno cout << "NO"   //"No"
+#define fastio ios_base::sync_with_stdio(0)
+#define untie cin.tie(0)
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define repe(i, n) for (int i = 1; i <= (n); i++)
+#define rrep(i, n) for (int i = (n)-1; i >= 0; i--)
+#define rrepe(i, n) for (int i = (n); i > 0; i--)
+#define ms(a, b) memset(a, b, sizeof a)
+#define a2z(x) (x).begin(), (x).end()
+#define z2a(x) (x).rbegin(), (x).rend()
+#define gsz(v) ((int)(v).size())
+#define MOD 1000000007 //998244353
+#define MAX 1000005
+
+#if !defined(__INTERACTIVE__) && !defined(__DEBUG__)
+#define endl "\n"
+#endif
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+using vb = vector<bool>;
+using vi = vector<int>;
+using vl = vector<ll>;
+using vpii = vector<pair<int, int>>;
+
+using vvb = vector<vector<bool>>;
+using vvi = vector<vector<int>>;
+using vvl = vector<vector<ll>>;
+using vvpii = vector<vector<pair<int, int>>>;
+
+using mii = map<int, int>;
+using umii = unordered_map<int, int>;
+using seti = set<int>;
+using useti = unordered_set<int>;
+
+class DSU
+{
+public:
+    int n;
+    std::vector<int> parent, subsetSize, val;
+    DSU(int numberOfElements) : n(numberOfElements)
+    {
+        parent = std::vector<int>(numberOfElements);
+        subsetSize = std::vector<int>(numberOfElements, 1);
+        std::iota(parent.begin(), parent.end(), 0);
+        val = std::vector<int>(numberOfElements);
+    }
+
+    int getRepresentative(int subset)
+    {
+        if (parent[subset] == subset)
+            return subset;
+        return parent[subset] = getRepresentative(parent[subset]);
+    }
+
+    bool isConnected(int subset1, int subset2)
+    {
+        return getRepresentative(subset1) == getRepresentative(subset2);
+    }
+
+    bool join(int subset1, int subset2)
+    {
+        if (isConnected(subset1, subset2))
+            return false;
+        subset1 = getRepresentative(subset1);
+        subset2 = getRepresentative(subset2);
+        if (subsetSize[subset1] < subsetSize[subset2])
+            swap(subset1, subset2);
+        subsetSize[subset1] += subsetSize[subset2];
+        parent[subset2] = subset1;
+        val[subset1] = min(val[subset1], val[subset2]);
+        return true;
+    }
+};
+
+inline void __run_test()
+{
+    int n, m;
+    ll res = 0LL;
+    cin >> n >> m;
+
+    DSU dsu(n);
+    rep(i, n) cin >> dsu.val[i];
+
+    vector<pair<int, pii>> edges;
+    rep(i, m)
+    {
+        int u, v, w;
+        cin >> u >> v >> w;
+        u--;
+        v--;
+        edges.pb({w, {u, v}});
+    }
+    sort(a2z(edges));
+    for (auto d : edges)
+        if (dsu.join(d.sc.fr, d.sc.sc))
+            res += d.fr;
+
+    vb visited(n);
+    vi all;
+    rep(i, n)
+    {
+        int r = dsu.getRepresentative(i);
+        if (visited[r])
+            continue;
+        visited[r] = 1;
+        all.pb(dsu.val[r]);
+    }
+    sort(a2z(all));
+    repe(i, gsz(all) - 1) res += all[i];
+
+    cout << res << endl;
+}
+
+int main()
+{
+    fastio;
+#if !defined(__INTERACTIVE__) && !defined(__DEBUG__)
+    untie;
+#ifdef LOCAL_OUTPUT
+    freopen(LOCAL_OUTPUT, "w", stdout);
+#endif
+#ifdef LOCAL_INPUT
+    freopen(LOCAL_INPUT, "r", stdin);
+#endif
+#endif
+    int __tests;
+    cin >> __tests;
+    while (__tests--)
+        __run_test();
+}
+
+/*
+-> Check for data types
+-> Fix macro
+-> Run test case
+-> Check the copied part
+-> Set or multiset?
+-> Check statement order
+*/
