0001ffd811f7e15c876b8d241812bec026149246
mwashief <muwashief@gmail.com>
2021-11-29 18:52:07 +0600

    Codeforces 1609D

---
+++ Codeforces/1501-1750/1609/1609D.cpp
@@ -0,0 +1,188 @@
+/*
+    Washief Hossain Mugdho
+    28 November 2021
+    Codeforces 1501-1750 1609 1609D
+*/
+
+#ifndef __DEBUG__
+#pragma GCC optimize("Ofast")
+#pragma GCC optimization("unroll-loops, no-stack-protector")
+#pragma GCC target("avx,avx2,fma")
+#endif
+
+#undef __INTERACTIVE__
+
+#include <bits/stdc++.h>
+#define pb push_back
+#define mp make_pair
+#define fr first
+#define sc second
+#define pyes cout << "YES" //"Yes"
+#define pno cout << "NO"   //"No"
+#define fastio ios_base::sync_with_stdio(0)
+#define untie cin.tie(0)
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define repe(i, n) for (int i = 1; i <= (n); i++)
+#define rrep(i, n) for (int i = (n)-1; i >= 0; i--)
+#define rrepe(i, n) for (int i = (n); i > 0; i--)
+#define ms(a, b) memset(a, b, sizeof a)
+#define a2z(x) (x).begin(), (x).end()
+#define z2a(x) (x).rbegin(), (x).rend()
+#define gsz(v) ((int)(v).size())
+#define MOD 1000000007 //998244353
+#define MAX 1000005
+
+#if !defined(__INTERACTIVE__) && !defined(__DEBUG__)
+#define endl "\n"
+#endif
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+using vb = vector<bool>;
+using vi = vector<int>;
+using vl = vector<ll>;
+using vpii = vector<pair<int, int>>;
+
+using vvb = vector<vector<bool>>;
+using vvi = vector<vector<int>>;
+using vvl = vector<vector<ll>>;
+using vvpii = vector<vector<pair<int, int>>>;
+
+using mii = map<int, int>;
+using umii = unordered_map<int, int>;
+using seti = set<int>;
+using useti = unordered_set<int>;
+class DSU
+{
+public:
+    int n;
+    std::vector<int> parent, subsetSize;
+    DSU(int numberOfElements) : n(numberOfElements)
+    {
+        parent = std::vector<int>(numberOfElements);
+        subsetSize = std::vector<int>(numberOfElements, 1);
+        std::iota(parent.begin(), parent.end(), 0);
+    }
+
+    int getRepresentative(int subset)
+    {
+        if (parent[subset] == subset)
+            return subset;
+        return parent[subset] = getRepresentative(parent[subset]);
+    }
+
+    bool isConnected(int subset1, int subset2)
+    {
+        return getRepresentative(subset1) == getRepresentative(subset2);
+    }
+
+    bool join(int subset1, int subset2)
+    {
+        if (isConnected(subset1, subset2))
+            return false;
+        subset1 = getRepresentative(subset1);
+        subset2 = getRepresentative(subset2);
+        if (subsetSize[subset1] < subsetSize[subset2])
+            swap(subset1, subset2);
+        subsetSize[subset1] += subsetSize[subset2];
+        parent[subset2] = subset1;
+        return true;
+    }
+};
+
+namespace std
+{
+
+    template <class Fun>
+    class y_combinator_result
+    {
+        Fun fun_;
+
+    public:
+        template <class T>
+        explicit y_combinator_result(T &&fun) : fun_(std::forward<T>(fun)) {}
+
+        template <class... Args>
+        decltype(auto) operator()(Args &&...args)
+        {
+            return fun_(std::ref(*this), std::forward<Args>(args)...);
+        }
+    };
+
+    template <class Fun>
+    decltype(auto) y_combinator(Fun &&fun)
+    {
+        return y_combinator_result<std::decay_t<Fun>>(std::forward<Fun>(fun));
+    }
+
+    template <class T>
+    T &unmove(T &&t) { return t; }
+
+}
+
+int main()
+{
+    fastio;
+#if !defined(__INTERACTIVE__) && !defined(__DEBUG__)
+    untie;
+#ifdef LOCAL_OUTPUT
+    freopen(LOCAL_OUTPUT, "w", stdout);
+#endif
+#ifdef LOCAL_INPUT
+    freopen(LOCAL_INPUT, "r", stdin);
+#endif
+#endif
+    int n, d;
+    cin >> n >> d;
+    vvi a(n);
+    DSU dsu(n);
+    int extra = 0;
+    while (d--)
+    {
+        int x, y;
+        cin >> x >> y;
+        x--;
+        y--;
+        a[x].pb(y);
+        a[y].pb(x);
+        if (!dsu.join(x, y))
+            extra++;
+        vi components;
+        vi visited(n);
+        rep(i, n) if (!visited[i])
+        {
+            visited[i] = true;
+            components.pb(0);
+            y_combinator([&](auto self, int node) -> void
+                         {
+                             components.back()++;
+                             for (auto k : a[node])
+                                 if (!visited[k])
+                                 {
+                                     visited[k] = 1;
+                                     self(k);
+                                 }
+                         })(i);
+        }
+        sort(a2z(components), greater<int>());
+        int res = 0;
+        for (int j = 0; j <= min(gsz(components) - 1, extra); j++)
+            res += components[j];
+        cout << res - 1 << endl;
+    }
+}
+
+/*
+-> Check for data types
+-> Fix macro
+-> Run test case
+-> Check the copied part
+-> Set or multiset?
+-> Check statement order
+*/
