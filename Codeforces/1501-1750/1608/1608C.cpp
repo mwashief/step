c81d4a033910a9602d0c4f61c00b16f460320d2b
mwashief <muwashief@gmail.com>
2021-12-11 18:51:42 +0600

    Codeforces 1608C

---
+++ Codeforces/1501-1750/1608/1608C.cpp
@@ -0,0 +1,136 @@
+/*
+    Washief Hossain Mugdho
+    11 December 2021
+    Codeforces 1501-1750 1608 1608C
+*/
+
+#ifndef __DEBUG__
+#pragma GCC optimize("Ofast")
+#pragma GCC optimization("unroll-loops, no-stack-protector")
+#pragma GCC target("avx,avx2,fma")
+#endif
+
+#undef __INTERACTIVE__
+
+#include <bits/stdc++.h>
+#define pb push_back
+#define mp make_pair
+#define fr first
+#define sc second
+#define pyes cout << "YES" //"Yes"
+#define pno cout << "NO"   //"No"
+#define fastio ios_base::sync_with_stdio(0)
+#define untie cin.tie(0)
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define repe(i, n) for (int i = 1; i <= (n); i++)
+#define rrep(i, n) for (int i = (n)-1; i >= 0; i--)
+#define rrepe(i, n) for (int i = (n); i > 0; i--)
+#define ms(a, b) memset(a, b, sizeof a)
+#define a2z(x) (x).begin(), (x).end()
+#define z2a(x) (x).rbegin(), (x).rend()
+#define gsz(v) ((int)(v).size())
+#define MOD 1000000007 //998244353
+#define MAX 1000005
+
+#if !defined(__INTERACTIVE__) && !defined(__DEBUG__)
+#define endl "\n"
+#endif
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+using vb = vector<bool>;
+using vi = vector<int>;
+using vl = vector<ll>;
+using vpii = vector<pair<int, int>>;
+
+using vvb = vector<vector<bool>>;
+using vvi = vector<vector<int>>;
+using vvl = vector<vector<ll>>;
+using vvpii = vector<vector<pair<int, int>>>;
+
+using mii = map<int, int>;
+using umii = unordered_map<int, int>;
+using seti = set<int>;
+using useti = unordered_set<int>;
+
+inline void __run_test()
+{
+    int n;
+    cin >> n;
+    vpii a(n), b(n);
+    rep(i, n)
+    {
+        cin >> a[i].fr;
+        a[i].sc = i;
+        b[i].sc = i;
+    }
+    rep(i, n) cin >> b[i].fr;
+    sort(a2z(a));
+    sort(a2z(b));
+    vi posa(n), posb(n);
+    rep(i, n)
+    {
+        posa[a[i].sc] = i;
+        posb[b[i].sc] = i;
+    }
+    vi aa(n), bb(n);
+    rep(i, n)
+    {
+        aa[i] = posb[a[i].sc];
+        bb[i] = posa[b[i].sc];
+    }
+    rrep(i, n - 1)
+    {
+        aa[i] = min(aa[i], aa[i + 1]);
+        bb[i] = min(bb[i], bb[i + 1]);
+    }
+    int ka = n - 1, kb = n - 1;
+    rep(i, 2 * n)
+    {
+        kb = aa[ka];
+        ka = bb[kb];
+    }
+    vi res(n);
+    rep(i, n)
+    {
+        if (i >= ka)
+            res[a[i].sc] = 1;
+        if (i >= kb)
+            res[b[i].sc] = 1;
+    }
+    rep(i, n) cout << res[i];
+    cout << endl;
+}
+
+int main()
+{
+    fastio;
+#if !defined(__INTERACTIVE__) && !defined(__DEBUG__)
+    untie;
+#ifdef LOCAL_OUTPUT
+    freopen(LOCAL_OUTPUT, "w", stdout);
+#endif
+#ifdef LOCAL_INPUT
+    freopen(LOCAL_INPUT, "r", stdin);
+#endif
+#endif
+    int __tests;
+    cin >> __tests;
+    while (__tests--)
+        __run_test();
+}
+
+/*
+-> Check for data types
+-> Fix macro
+-> Run test case
+-> Check the copied part
+-> Set or multiset?
+-> Check statement order
+*/
