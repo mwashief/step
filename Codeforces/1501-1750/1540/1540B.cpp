1f36c725be935822ca5aeaf69e485e389039ffdc
mwashief <muwashief@gmail.com>
2021-12-12 10:13:16 +0600

    Codeforces 1540B

---
+++ Codeforces/1501-1750/1540/1540B.cpp
@@ -0,0 +1,263 @@
+/*
+    Washief Hossain Mugdho
+    11 December 2021
+    Codeforces 1501-1750 1540 1540B
+*/
+// https://codeforces.com/problemset/problem/1540/B
+
+#ifndef __DEBUG__
+#pragma GCC optimize("Ofast")
+#pragma GCC optimization("unroll-loops, no-stack-protector")
+#pragma GCC target("avx,avx2,fma")
+#endif
+
+#undef __INTERACTIVE__
+
+#include <bits/stdc++.h>
+#define pb push_back
+#define mp make_pair
+#define fr first
+#define sc second
+#define pyes cout << "YES" //"Yes"
+#define pno cout << "NO"   //"No"
+#define fastio ios_base::sync_with_stdio(0)
+#define untie cin.tie(0)
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define repe(i, n) for (int i = 1; i <= (n); i++)
+#define rrep(i, n) for (int i = (n)-1; i >= 0; i--)
+#define rrepe(i, n) for (int i = (n); i > 0; i--)
+#define ms(a, b) memset(a, b, sizeof a)
+#define a2z(x) (x).begin(), (x).end()
+#define z2a(x) (x).rbegin(), (x).rend()
+#define gsz(v) ((int)(v).size())
+#define MOD 1000000007 //998244353
+#define MAX 1000005
+
+#if !defined(__INTERACTIVE__) && !defined(__DEBUG__)
+#define endl "\n"
+#endif
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+using vb = vector<bool>;
+using vi = vector<int>;
+using vl = vector<ll>;
+using vpii = vector<pair<int, int>>;
+
+using vvb = vector<vector<bool>>;
+using vvi = vector<vector<int>>;
+using vvl = vector<vector<ll>>;
+using vvpii = vector<vector<pair<int, int>>>;
+
+using mii = map<int, int>;
+using umii = unordered_map<int, int>;
+using seti = set<int>;
+using useti = unordered_set<int>;
+
+template <int mod>
+struct Mod
+{
+    long long x;
+    Mod() : x(0) {}
+    Mod(long long xx) : x(xx)
+    {
+        if (abs(x) >= mod)
+            x %= mod;
+        if (x < 0)
+            x += mod;
+    }
+    operator int() const { return x; }
+    operator long long() const { return x; }
+    Mod operator+(const Mod &a) const
+    {
+        Mod n;
+        n.x = x + a.x;
+        if (n.x >= mod)
+            n.x -= mod;
+        return n;
+    }
+    Mod operator-(const Mod &a) const
+    {
+        Mod n;
+        n.x = x - a.x;
+        if (n.x < 0)
+            n.x += mod;
+        return n;
+    }
+    Mod operator*(const Mod &a) const
+    {
+        return x * a.x;
+    }
+    Mod operator+=(const Mod &a)
+    {
+        x += a.x;
+        if (x >= mod)
+            x -= mod;
+        return *this;
+    }
+    Mod operator-=(const Mod &a)
+    {
+        x -= a.x;
+        if (x < 0)
+            x += mod;
+        return *this;
+    }
+    Mod operator++()
+    {
+        *this += 1;
+        return *this;
+    }
+    Mod operator++(int) //postfix
+    {
+        Mod temp = *this;
+        *this += 1;
+        return temp;
+    }
+    Mod operator--()
+    {
+        *this -= 1;
+        return *this;
+    }
+    Mod operator--(int) //postfix
+    {
+        Mod temp = *this;
+        *this -= 1;
+        return temp;
+    }
+    Mod operator*=(const Mod &a)
+    {
+        x = (x * a.x) % mod;
+        return *this;
+    }
+    Mod pow(long long b) const
+    {
+        Mod ans = 1;
+        Mod a = *this;
+        while (b > 0)
+        {
+            if (b & 1)
+                ans *= a;
+            a *= a;
+            b /= 2;
+        }
+        return ans;
+    }
+    Mod inv() const
+    {
+        return pow(mod - 2);
+    }
+    Mod operator/(const Mod &a) const
+    {
+        return (*this) * a.inv();
+    }
+    Mod operator/=(const Mod &a)
+    {
+        return (*this) *= a.inv();
+    }
+    bool operator==(const Mod &o) const
+    {
+        return x == o.x;
+    }
+    bool operator!=(const Mod &o) const
+    {
+        return x != o.x;
+    }
+    long long operator()() const
+    {
+        return x;
+    }
+    template <int _mod>
+    friend ostream &operator<<(ostream &os, const Mod<_mod> &num)
+    {
+        os << num.x;
+        return os;
+    }
+    template <int _mod>
+    friend istream &operator>>(istream &is, Mod<_mod> &num)
+    {
+        is >> num.x;
+        if (abs(num.x) >= mod)
+            num.x %= mod;
+        if (num.x < 0)
+            num.x += mod;
+        return is;
+    }
+};
+using base = Mod<MOD>;
+using vbase = vector<base>;
+using vvbase = vector<vector<base>>;
+base two = 2;
+
+int memo[220][220];
+
+base dp(int a, int b)
+{
+    if (a == 0)
+        return base(1);
+    if (b == 0)
+        return base(0);
+    if (memo[a][b] != -1)
+        return base(memo[a][b]);
+    base res = dp(a - 1, b) + dp(a, b - 1);
+    res /= two;
+    memo[a][b] = res.x;
+    return res;
+}
+
+int main()
+{
+    fastio;
+#if !defined(__INTERACTIVE__) && !defined(__DEBUG__)
+    untie;
+#ifdef LOCAL_OUTPUT
+    freopen(LOCAL_OUTPUT, "w", stdout);
+#endif
+#ifdef LOCAL_INPUT
+    freopen(LOCAL_INPUT, "r", stdin);
+#endif
+#endif
+    ms(memo, -1);
+    int n;
+    cin >> n;
+    vvi adj(n, vi(n, n + 1));
+    rep(i, n - 1)
+    {
+        int u, v;
+        cin >> u >> v;
+        u--;
+        v--;
+        adj[u][v] = 1;
+        adj[v][u] = 1;
+    }
+    rep(i, n) adj[i][i] = 0;
+    rep(k, n) rep(i, n) rep(j, n) adj[i][j] = min(adj[i][j], adj[i][k] + adj[k][j]);
+
+    base res = 0;
+    rep(i, n) for (int j = i + 1; j < n; j++) rep(k, n)
+    {
+        if (adj[k][j] + adj[j][i] == adj[k][i])
+            res += 1;
+        else if (adj[k][i] + adj[i][j] != adj[k][j])
+        {
+            int dis = (adj[k][i] + adj[k][j] - adj[i][j]) >> 1;
+            res += dp(adj[k][j] - dis, adj[k][i] - dis);
+        }
+    }
+    res = res / base(n);
+    cout << res << endl;
+}
+
+/*
+-> Check for data types
+-> Fix macro
+-> Run test case
+-> Check the copied part
+-> Set or multiset?
+-> Check statement order
+*/
