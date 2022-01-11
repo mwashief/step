a75836ed4febc61be53823f15017420bca23f26c
mwashief <muwashief@gmail.com>
2022-01-11 19:05:08 +0600

    Codeforces 1624E

---
+++ Codeforces/1501-1750/1624/1624E.cpp
@@ -0,0 +1,169 @@
+/*
+    Washief Hossain Mugdho
+    10 January 2022
+    Codeforces 1501-1750 1624 1624E
+*/
+
+#ifndef __DEBUG__
+#pragma GCC optimize("Ofast")
+#pragma GCC optimization("unroll-loops, no-stack-protector")
+#pragma GCC target("avx,avx2,fma")
+#endif
+
+#undef __INTERACTIVE__
+
+#include <bits/stdc++.h>
+#define pb push_back
+#define mp make_pair
+#define fr first
+#define sc second
+#define pyes cout << "YES" //"Yes"
+#define pno cout << "NO"   //"No"
+#define fastio ios_base::sync_with_stdio(0)
+#define untie cin.tie(0)
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define repe(i, n) for (int i = 1; i <= (n); i++)
+#define rrep(i, n) for (int i = (n)-1; i >= 0; i--)
+#define rrepe(i, n) for (int i = (n); i > 0; i--)
+#define ms(a, b) memset(a, b, sizeof a)
+#define a2z(x) (x).begin(), (x).end()
+#define z2a(x) (x).rbegin(), (x).rend()
+#define gsz(v) ((int)(v).size())
+#define MOD 1000000007 //998244353
+#define MAX 1000005
+
+#if !defined(__INTERACTIVE__) && !defined(__DEBUG__)
+#define endl "\n"
+#endif
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+using vb = vector<bool>;
+using vi = vector<int>;
+using vl = vector<ll>;
+using vpii = vector<pair<int, int>>;
+
+using vvb = vector<vector<bool>>;
+using vvi = vector<vector<int>>;
+using vvl = vector<vector<ll>>;
+using vvpii = vector<vector<pair<int, int>>>;
+
+using mii = map<int, int>;
+using umii = unordered_map<int, int>;
+using seti = set<int>;
+using useti = unordered_set<int>;
+
+int n, m;
+pair<pii, int> path[1004];
+int memo[1004];
+string all[1004];
+string s;
+map<string, pair<pii, int>> snippet;
+
+bool dp(int ind)
+{
+    if (ind == m)
+        return true;
+    if (ind == m - 1)
+        return false;
+    if (memo[ind] != -1)
+        return memo[ind];
+    string t;
+    t.pb(s[ind]);
+    t.pb(s[ind + 1]);
+    bool res = false;
+    if (snippet.find(t) != snippet.end())
+    {
+        if (dp(ind + 2))
+        {
+            path[ind] = snippet[t];
+            return memo[ind] = 1;
+        }
+    }
+    if (ind < m - 2)
+    {
+        t.pb(s[ind + 2]);
+        if (snippet.find(t) != snippet.end())
+            if (dp(ind + 3))
+            {
+                path[ind] = snippet[t];
+                return memo[ind] = 1;
+            }
+    }
+    return memo[ind] = 0;
+}
+
+inline void __run_test()
+{
+    cin >> n >> m;
+    rep(i, n) cin >> all[i];
+    cin >> s;
+    snippet.clear();
+    rep(j, n)
+    {
+        rep(i, m - 1)
+        {
+            string t;
+            t.pb(all[j][i]);
+            t.pb(all[j][i + 1]);
+            snippet[t] = {{i + 1, i + 2}, j + 1};
+            if (i < m - 2)
+            {
+                t.pb(all[j][i + 2]);
+                snippet[t] = {{i + 1, i + 3}, j + 1};
+            }
+        }
+    }
+    rep(i, m + 2) memo[i] = -1;
+    if (dp(0))
+    {
+        int ind = 0;
+        vector<pair<pii, int>> res;
+        while (ind < m)
+        {
+            auto k = path[ind];
+            res.pb(k);
+            ind += k.fr.sc - k.fr.fr + 1;
+        }
+        cout << gsz(res) << endl;
+        for (auto k : res)
+            cout << k.fr.fr << " " << k.fr.sc << " " << k.sc << endl;
+    }
+    else
+        cout << -1 << endl;
+}
+
+int main()
+{
+    fastio;
+#if !defined(__INTERACTIVE__) && !defined(__DEBUG__)
+    untie;
+#ifdef LOCAL_OUTPUT
+    freopen(LOCAL_OUTPUT, "w", stdout);
+#endif
+#ifdef LOCAL_INPUT
+    freopen(LOCAL_INPUT, "r", stdin);
+#endif
+#endif
+    int __tests;
+    cin >> __tests;
+    while (__tests--)
+        __run_test();
+}
+
+/*
+-> Check for data types
+-> Fix macro
+-> Run test case
+-> Check the copied part
+-> Set or multiset?
+-> Check statement order
+-> Check simple logic errors
+-> Read problem statements carefully
+*/
