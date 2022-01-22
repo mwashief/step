7b9c6933784476252fe4cad23471b583a68ceed1
mwashief <muwashief@gmail.com>
2022-01-22 23:54:30 +0600

    Codeforces 1629C

---
+++ Codeforces/1501-1750/1629/1629C.cpp
@@ -0,0 +1,206 @@
+/*
+    Washief Hossain Mugdho
+    22 January 2022
+    Codeforces 1501-1750 1629 1629C
+*/
+
+#ifndef __DEBUG__
+#pragma GCC optimize("Ofast")
+#pragma GCC optimization("unroll-loops, no-stack-protector")
+#pragma GCC target("avx,avx2,fma")
+#endif
+
+#undef __INTERACTIVE__
+
+#include <bits/stdc++.h>
+#define pb push_back
+#define mp make_pair
+#define fr first
+#define sc second
+#define pyes cout << "YES" //"Yes"
+#define pno cout << "NO"   //"No"
+#define fastio ios_base::sync_with_stdio(0)
+#define untie cin.tie(0)
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define repe(i, n) for (int i = 1; i <= (n); i++)
+#define rrep(i, n) for (int i = (n)-1; i >= 0; i--)
+#define rrepe(i, n) for (int i = (n); i > 0; i--)
+#define ms(a, b) memset(a, b, sizeof a)
+#define a2z(x) (x).begin(), (x).end()
+#define z2a(x) (x).rbegin(), (x).rend()
+#define gsz(v) ((int)(v).size())
+#define MOD 1000000007 //998244353
+#define MAX 1000005
+
+#if !defined(__INTERACTIVE__) && !defined(__DEBUG__)
+#define endl "\n"
+#endif
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+using vb = vector<bool>;
+using vi = vector<int>;
+using vl = vector<ll>;
+using vpii = vector<pair<int, int>>;
+
+using vvb = vector<vector<bool>>;
+using vvi = vector<vector<int>>;
+using vvl = vector<vector<ll>>;
+using vvpii = vector<vector<pair<int, int>>>;
+
+using mii = map<int, int>;
+using umii = unordered_map<int, int>;
+using seti = set<int>;
+using useti = unordered_set<int>;
+
+template <class T, class OP>
+class SegmentTree
+{
+    vector<T> t;
+    void build()
+    {
+        for (int i = n - 1; i > 0; --i)
+            t[i] = OP()(t[i << 1], t[i << 1 | 1]);
+    }
+
+public:
+    int n;
+
+    SegmentTree(int nn)
+    {
+        n = nn;
+        t = vector<T>(n << 1);
+    }
+
+    SegmentTree(vector<T> v)
+    {
+        n = v.size();
+        t = vector<T>(n << 1);
+        for (int i = 0; i < n; ++i)
+            t[i + n] = v[i];
+        build();
+    }
+
+    void update(int p, T value)
+    {
+        for (t[p += n] = value; p > 1; p >>= 1)
+            t[p >> 1] = OP()(t[p], t[p ^ 1]);
+    }
+
+    T query(int l, int r)
+    {
+        r++;
+        T res = OP::initVal;
+        for (l += n, r += n; l < r; l >>= 1, r >>= 1)
+        {
+            if (l & 1)
+                res = OP()(res, t[l++]);
+            if (r & 1)
+                res = OP()(res, t[--r]);
+        }
+        return res;
+    }
+};
+
+template <class T>
+class Add
+{
+public:
+    static const T initVal = T(0);
+    T operator()(T x, T y)
+    {
+        return x + y;
+    }
+};
+
+inline void __run_test()
+{
+    int n;
+    cin >> n;
+    vi a(n);
+    rep(i, n) cin >> a[i];
+    SegmentTree<int, Add<int>> st(n + 1);
+    vi suffix(n);
+    rrep(i, n)
+    {
+        st.update(a[i], 1);
+        int l = -1, r = n;
+        while (l < r)
+        {
+            int mid = (l + r + 1) >> 1;
+            if (st.query(0, mid) == mid + 1)
+                l = mid;
+            else
+                r = mid - 1;
+        }
+        suffix[i] = l + 1;
+    }
+    SegmentTree<int, Add<int>> s(n + 1);
+    vi res;
+    int start = 0;
+    while (start < n)
+    {
+        int j = start;
+        int need = suffix[start];
+        while (j < n)
+        {
+            s.update(a[j], 1);
+            int l = -1, r = n;
+            while (l < r)
+            {
+                int mid = (l + r + 1) >> 1;
+                if (s.query(0, mid) == mid + 1)
+                    l = mid;
+                else
+                    r = mid - 1;
+            }
+            l++;
+            if (l == need)
+                break;
+            j++;
+        }
+        res.pb(need);
+        for (int i = start; i <= j; i++)
+            s.update(a[i], 0);
+        start = j + 1;
+    }
+    cout << gsz(res) << endl;
+    for (auto d : res)
+        cout << d << " ";
+    cout << endl;
+}
+
+int main()
+{
+    fastio;
+#if !defined(__INTERACTIVE__) && !defined(__DEBUG__)
+    untie;
+#ifdef LOCAL_OUTPUT
+    freopen(LOCAL_OUTPUT, "w", stdout);
+#endif
+#ifdef LOCAL_INPUT
+    freopen(LOCAL_INPUT, "r", stdin);
+#endif
+#endif
+    int __tests;
+    cin >> __tests;
+    while (__tests--)
+        __run_test();
+}
+
+/*
+-> Check for data types
+-> Fix macro
+-> Run test case
+-> Check the copied part
+-> Set or multiset?
+-> Check statement order
+-> Check simple logic errors
+-> Read problem statements carefully
+*/
