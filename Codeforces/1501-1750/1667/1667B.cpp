b6cd735088899d631c1abc3605234005259bb2af
mwashief <muwashief@gmail.com>
2022-04-22 21:28:14 +0600

    Codeforces 1667B

---
+++ Codeforces/1501-1750/1667/1667B.cpp
@@ -0,0 +1,214 @@
+/*
+    Washief Hossain Mugdho
+    22 April 2022
+    Codeforces 1501-1750 1667 1667B
+*/
+
+#ifndef __DEBUG__
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+#endif
+
+#undef __INTERACTIVE__
+
+#include <bits/stdc++.h>
+#define pb push_back
+#define mp make_pair
+#define fr first
+#define sc second
+#define pyes cout << "YES" //"Yes"
+#define pno cout << "NO"   //"No"
+#define fastio ios_base::sync_with_stdio(0)
+#define untie cin.tie(0)
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define repe(i, n) for (int i = 1; i <= (n); i++)
+#define rrep(i, n) for (int i = (n)-1; i >= 0; i--)
+#define rrepe(i, n) for (int i = (n); i > 0; i--)
+#define ms(a, b) memset(a, b, sizeof a)
+#define a2z(x) (x).begin(), (x).end()
+#define z2a(x) (x).rbegin(), (x).rend()
+#define gsz(v) ((int)(v).size())
+#define MOD 1000000007 // 998244353
+#define MAX 1000005
+
+#if !defined(__INTERACTIVE__) && !defined(__DEBUG__)
+#define endl "\n"
+#endif
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+using vb = vector<bool>;
+using vi = vector<int>;
+using vl = vector<ll>;
+using vpii = vector<pair<int, int>>;
+
+using vvb = vector<vector<bool>>;
+using vvi = vector<vector<int>>;
+using vvl = vector<vector<ll>>;
+using vvpii = vector<vector<pair<int, int>>>;
+
+using mii = map<int, int>;
+using umii = unordered_map<int, int>;
+using seti = set<int>;
+using useti = unordered_set<int>;
+
+template <class T, class OP>
+class SegmentTree
+{
+    vector<T> t;
+    void build()
+    {
+        for (int i = n - 1; i > 0; --i)
+            t[i] = OP()(t[i << 1], t[i << 1 | 1]);
+    }
+
+public:
+    int n;
+
+    SegmentTree(int nn)
+    {
+        n = nn;
+        t = vector<T>(n << 1, -1000000);
+    }
+
+    SegmentTree(vector<T> v)
+    {
+        n = v.size();
+        t = vector<T>(n << 1);
+        for (int i = 0; i < n; ++i)
+            t[i + n] = v[i];
+        build();
+    }
+
+    void update(int p, T value)
+    {
+        for (t[p += n] = value; p > 1; p >>= 1)
+            t[p >> 1] = OP()(t[p], t[p ^ 1]);
+    }
+
+    T query(int l, int r)
+    {
+        r++;
+        T res = OP::initVal;
+        for (l += n, r += n; l < r; l >>= 1, r >>= 1)
+        {
+            if (l & 1)
+                res = OP()(res, t[l++]);
+            if (r & 1)
+                res = OP()(res, t[--r]);
+        }
+        return res;
+    }
+
+    void print()
+    {
+        for (int i = 0; i < n; i++)
+            cout << query(i, i) << " ";
+        cout << endl;
+    }
+};
+
+template <class T>
+class Max
+{
+public:
+    static const T initVal = -1000000;
+    T operator()(T x, T y)
+    {
+        return max(x, y);
+    }
+};
+
+template <class T>
+class Min
+{
+public:
+    static const T initVal = numeric_limits<T>::max();
+    T operator()(T x, T y)
+    {
+        return min(x, y);
+    }
+};
+
+template <class T>
+class Add
+{
+public:
+    static const T initVal = T(0);
+    T operator()(T x, T y)
+    {
+        return x + y;
+    }
+};
+
+inline void __run_test()
+{
+    int n;
+    cin >> n;
+    vi a(n);
+    rep(i, n) cin >> a[i];
+    vl prefix(n + 1);
+    repe(i, n) prefix[i] += prefix[i - 1] + a[i - 1];
+
+    vl temp = prefix;
+    sort(a2z(temp));
+
+    rep(i, n + 1) prefix[i] = lower_bound(a2z(temp), prefix[i]) - temp.begin();
+    vi dp(n + 1, -n);
+    dp[n] = 0;
+
+    map<int, int> same;
+    SegmentTree<int, Max<int>> st(n + 2);
+    st.update(prefix[n], n - 1);
+
+    rrep(i, n)
+    {
+        int x = 0;
+        if (a[i] > 0)
+            x = 1;
+        else if (a[i] < 0)
+            x = -1;
+        dp[i] = x + dp[i + 1];
+        int p = prefix[i];
+        // st.print();
+        dp[i] = max(dp[i], st.query(p + 1, n + 1) - i + 1);
+
+        st.update(prefix[i], max(st.query(prefix[i], prefix[i]), dp[i] + i - 1));
+    }
+    cout << dp[0] << endl;
+}
+
+int main()
+{
+    fastio;
+#if !defined(__INTERACTIVE__) && !defined(__DEBUG__)
+    untie;
+#ifdef LOCAL_OUTPUT
+    freopen(LOCAL_OUTPUT, "w", stdout);
+#endif
+#ifdef LOCAL_INPUT
+    freopen(LOCAL_INPUT, "r", stdin);
+#endif
+#endif
+    int __tests;
+    cin >> __tests;
+    while (__tests--)
+        __run_test();
+}
+
+/*
+-> Check for data types
+-> Fix macro
+-> Run test case
+-> Check the copied part
+-> Set or multiset?
+-> Check statement order
+-> Check simple logic errors
+-> Read problem statements carefully
+*/
