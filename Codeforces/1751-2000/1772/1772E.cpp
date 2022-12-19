43d7bc8f21d199b38e64d65b3c9f9946cce5fad2
Washief Mugdho <muwashief@gmail.com>
2022-12-19 17:14:17 +0600

    Codeforces 1772E

---
+++ Codeforces/1751-2000/1772/1772E.cpp
@@ -0,0 +1,112 @@
+/*
+    18 December 2022
+    Codeforces 1751-2000 1772 1772E
+*/
+
+#ifndef __DEBUG__
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+#endif
+
+#undef __INTERACTIVE__
+
+#include <bits/stdc++.h>
+#define pb push_back
+#define mp make_pair
+#define fr first
+#define sc second
+#define pyes cout << "YES" //"Yes"
+#define pno cout << "NO"   //"No"
+#define fastio ios_base::sync_with_stdio(0)
+#define untie cin.tie(0)
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define repe(i, n) for (int i = 1; i <= (n); i++)
+#define rrep(i, n) for (int i = (n)-1; i >= 0; i--)
+#define rrepe(i, n) for (int i = (n); i > 0; i--)
+#define ms(a, b) memset(a, b, sizeof a)
+#define a2z(x) (x).begin(), (x).end()
+#define z2a(x) (x).rbegin(), (x).rend()
+#define gsz(v) ((int)(v).size())
+#define MOD 1000000007 // 998244353
+#define MAX 1000005
+
+#if !defined(__INTERACTIVE__) && !defined(__DEBUG__)
+#define endl "\n"
+#endif
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+using vb = vector<bool>;
+using vi = vector<int>;
+using vl = vector<ll>;
+using vpii = vector<pair<int, int>>;
+
+using vvb = vector<vector<bool>>;
+using vvi = vector<vector<int>>;
+using vvl = vector<vector<ll>>;
+using vvpii = vector<vector<pair<int, int>>>;
+
+using mii = map<int, int>;
+using umii = unordered_map<int, int>;
+using seti = set<int>;
+using useti = unordered_set<int>;
+
+inline void __run_test()
+{
+    int n;
+    cin >> n;
+    vi a(n + 1);
+    repe(i, n) cin >> a[i];
+    int only1 = 0, only2 = 0, nobodys = 0;
+    repe(i, n)
+    {
+        if (a[i] == i && a[i] != n - i + 1)
+            only1++;
+        if (a[i] != i && a[i] == n - i + 1)
+            only2++;
+        if (a[i] != i && a[i] != n - i + 1)
+            nobodys++;
+    }
+
+    if (only1 >= nobodys + only2)
+        cout << "First" << endl;
+    else if (only2 > nobodys + only1)
+        cout << "Second" << endl;
+    else
+        cout << "Tie" << endl;
+}
+
+int main()
+{
+    fastio;
+#if !defined(__INTERACTIVE__) && !defined(__DEBUG__)
+    untie;
+#ifdef LOCAL_OUTPUT
+    freopen(LOCAL_OUTPUT, "w", stdout);
+#endif
+#ifdef LOCAL_INPUT
+    freopen(LOCAL_INPUT, "r", stdin);
+#endif
+#endif
+    int __tests;
+    cin >> __tests;
+    while (__tests--)
+        __run_test();
+}
+
+/*
+-> Check for data types
+-> Fix macro
+-> Run test case
+-> Check the copied part
+-> Set or multiset?
+-> Check statement order
+-> Check simple logic errors
+-> Read problem statements carefully
+*/
