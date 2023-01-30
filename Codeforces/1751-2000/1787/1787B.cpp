2ac63abc89c5f0006b2c7dc6405aae9748cab7e4
Washief Mugdho <muwashief@gmail.com>
2023-01-30 21:27:35 +0600

    .. 1787B

---
+++ Codeforces/1751-2000/1787/1787B.cpp
@@ -0,0 +1,130 @@
+/*
+    29 January 2023
+    Codeforces 1751-1800 1787 1787B
+*/
+
+#ifndef __DEBUG__
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+#endif
+
+#undef __INTERACTIVE__
+
+#include <bits/stdc++.h>
+#define pb push_back
+#define mp make_pair
+#define fr first
+#define sc second
+#define pyes cout << "YES" //"Yes"
+#define pno cout << "NO"   //"No"
+#define fastio ios_base::sync_with_stdio(0)
+#define untie cin.tie(0)
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define repe(i, n) for (int i = 1; i <= (n); i++)
+#define rrep(i, n) for (int i = (n)-1; i >= 0; i--)
+#define rrepe(i, n) for (int i = (n); i > 0; i--)
+#define ms(a, b) memset(a, b, sizeof a)
+#define a2z(x) (x).begin(), (x).end()
+#define z2a(x) (x).rbegin(), (x).rend()
+#define gsz(v) ((int)(v).size())
+#define MOD 1000000007 // 998244353
+#define MAX 1000005
+
+#if !defined(__INTERACTIVE__) && !defined(__DEBUG__)
+#define endl "\n"
+#endif
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+using vb = vector<bool>;
+using vi = vector<int>;
+using vl = vector<ll>;
+using vpii = vector<pair<int, int>>;
+
+using vvb = vector<vector<bool>>;
+using vvi = vector<vector<int>>;
+using vvl = vector<vector<ll>>;
+using vvpii = vector<vector<pair<int, int>>>;
+
+using mii = map<int, int>;
+using umii = unordered_map<int, int>;
+using seti = set<int>;
+using useti = unordered_set<int>;
+
+inline void __run_test()
+{
+    int n;
+    cin >> n;
+    vector<pair<ll, int>> all;
+    for (int i = 2; i * i <= n; i++)
+    {
+        int cnt = 0;
+        while (n % i == 0)
+        {
+            cnt++;
+            n /= i;
+        }
+
+        if (cnt)
+            all.pb({i, cnt});
+    }
+    if (n != 1)
+        all.pb({n, 1});
+    sort(a2z(all),
+         [&](auto a, auto b) -> bool
+         {
+             return a.sc < b.sc;
+         });
+
+    rrep(i, gsz(all) - 1) all[i].fr *= all[i + 1].fr;
+
+    int last = 0;
+    int i = 0;
+    ll res = 0;
+    while (i < gsz(all))
+    {
+        if (all[i].sc > last)
+        {
+            res += all[i].fr * (all[i].sc - last);
+            last = all[i].sc;
+        }
+        i++;
+    }
+
+    cout << res << endl;
+}
+
+int main()
+{
+    fastio;
+#if !defined(__INTERACTIVE__) && !defined(__DEBUG__)
+    untie;
+#ifdef LOCAL_OUTPUT
+    freopen(LOCAL_OUTPUT, "w", stdout);
+#endif
+#ifdef LOCAL_INPUT
+    freopen(LOCAL_INPUT, "r", stdin);
+#endif
+#endif
+    int __tests;
+    cin >> __tests;
+    while (__tests--)
+        __run_test();
+}
+
+/*
+-> Check for data types
+-> Fix macro
+-> Run test case
+-> Check the copied part
+-> Set or multiset?
+-> Check statement order
+-> Check simple logic errors
+-> Read problem statements carefully
+*/
