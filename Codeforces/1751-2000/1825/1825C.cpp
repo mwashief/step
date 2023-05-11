2ecc2273af31a38b5af2ec5862d93a0215b1bd23
Washief Mugdho <muwashief@gmail.com>
2023-05-11 17:12:38 +0100

    Codeforces 1825C

---
+++ Codeforces/1751-2000/1825/1825C.cpp
@@ -0,0 +1,127 @@
+/*
+    08 May 2023
+    Codeforces 1751-2000 1825 1825C
+*/
+
+#ifndef __DEBUG__
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+#endif
+
+#undef __INTERACTIVE__
+
+#include <bits/stdc++.h>
+#define pb push_back
+#define mp make_pair
+#define fr first
+#define sc second
+#define pyes cout << "YES" //"Yes"
+#define pno cout << "NO"   //"No"
+#define fastio ios_base::sync_with_stdio(0)
+#define untie cin.tie(0)
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define repe(i, n) for (int i = 1; i <= (n); i++)
+#define rrep(i, n) for (int i = (n)-1; i >= 0; i--)
+#define rrepe(i, n) for (int i = (n); i > 0; i--)
+#define ms(a, b) memset(a, b, sizeof a)
+#define a2z(x) (x).begin(), (x).end()
+#define z2a(x) (x).rbegin(), (x).rend()
+#define gsz(v) ((int)(v).size())
+#define MOD 1000000007 // 998244353
+#define MAX 1000005
+
+#if !defined(__INTERACTIVE__) && !defined(__DEBUG__)
+#define endl "\n"
+#endif
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+using vb = vector<bool>;
+using vi = vector<int>;
+using vl = vector<ll>;
+using vpii = vector<pair<int, int>>;
+
+using vvb = vector<vector<bool>>;
+using vvi = vector<vector<int>>;
+using vvl = vector<vector<ll>>;
+using vvpii = vector<vector<pair<int, int>>>;
+
+using mii = map<int, int>;
+using umii = unordered_map<int, int>;
+using seti = set<int>;
+using useti = unordered_set<int>;
+
+inline void __run_test()
+{
+    int n, m;
+    cin >> n >> m;
+    vi sides = {0, 0};
+    seti x;
+    rep(i, n)
+    {
+        int d;
+        cin >> d;
+        if (d < 0)
+            sides[d + 2]++;
+        else
+            x.insert(d);
+    }
+    vi gaps = {0};
+    int last = 0;
+    for (auto d : x)
+    {
+        int now = d - last - 1;
+        if (now > 0)
+            gaps.pb(now);
+        last = d;
+    }
+    if (last < m)
+        gaps.pb(m - last);
+    int right = accumulate(a2z(gaps), 0);
+    int left = 0;
+    int res = 0;
+
+    for (auto gap : gaps)
+    {
+        left += gap;
+        right -= gap;
+        res = max(res, min(left, sides[1]) + min(right, sides[0]));
+    }
+
+    cout << res + gsz(x) << endl;
+}
+
+int main()
+{
+    fastio;
+#if !defined(__INTERACTIVE__) && !defined(__DEBUG__)
+    untie;
+#ifdef LOCAL_OUTPUT
+    freopen(LOCAL_OUTPUT, "w", stdout);
+#endif
+#ifdef LOCAL_INPUT
+    freopen(LOCAL_INPUT, "r", stdin);
+#endif
+#endif
+    int __tests;
+    cin >> __tests;
+    while (__tests--)
+        __run_test();
+}
+
+/*
+-> Check for data types
+-> Fix macro
+-> Run test case
+-> Check the copied part
+-> Set or multiset?
+-> Check statement order
+-> Check simple logic errors
+-> Read problem statements carefully
+*/
